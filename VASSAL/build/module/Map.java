26aa6776e446a6d0b9a8865b549fb92417a7b18f
swampwallaby <swampwallaby@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2006-10-10 07:39:32 +0000

    Improved version Selection Highlighters Improve Performance of Zone Grid Handling
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@1290 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- VASSAL/build/module/Map.java
+++ VASSAL/build/module/Map.java
@@ -98,6 +98,7 @@ import VASSAL.build.module.map.PieceCollection;
 import VASSAL.build.module.map.PieceMover;
 import VASSAL.build.module.map.PieceRecenterer;
 import VASSAL.build.module.map.Scroller;
+import VASSAL.build.module.map.SelectionHighlighters;
 import VASSAL.build.module.map.SetupStack;
 import VASSAL.build.module.map.StackExpander;
 import VASSAL.build.module.map.StackMetrics;
@@ -411,6 +412,9 @@ public class Map extends AbstractConfigurable implements GameComponent, FocusLis
 		if (!getComponents(GlobalProperties.class).hasMoreElements()) {
 			addChild(new GlobalProperties());
 		}
+    if (!getComponents(SelectionHighlighters.class).hasMoreElements()) {
+      addChild(new SelectionHighlighters());
+    }
 		setup(false);
 	}
 
