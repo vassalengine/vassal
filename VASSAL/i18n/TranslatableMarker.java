5b88b24acd3f5a59de32da3b4d4120ee56e8ac87
rodneykinney <rodneykinney@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2007-05-27 03:58:42 +0000

    Add missing file from previous merge
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@2113 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ VASSAL/i18n/TranslatableMarker.java
@@ -0,0 +1,101 @@
+/*
+ * $Id: TranslatableMarker.java 1417 2006-11-03 14:57:33 +0000 (Fri, 03 Nov 2006) rodneykinney $
+ *
+ * Copyright (c) 2000-2006 by Rodney Kinney, Brent Easton
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+package VASSAL.i18n;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import VASSAL.build.AbstractConfigurable;
+import VASSAL.build.Buildable;
+import VASSAL.build.module.documentation.HelpFile;
+import VASSAL.counters.GamePiece;
+import VASSAL.counters.PlaceMarker;
+
+/**
+ * 
+ * @author Brent Easton
+ *
+ * A dummy AbstractConfigurable to hold a PlaceMarker or Replace definition while translating
+ */
+public class TranslatableMarker extends AbstractConfigurable {
+
+  protected GamePiece markerDefinition;
+  protected String[] names;
+  protected String[] values;
+  
+  public TranslatableMarker(PlaceMarker p) {
+    markerDefinition = p.createBaseMarker();
+    setConfigureName(p.getDescription());
+    
+  }
+  
+  public static String getConfigureTypeName() {
+    return "Marker Definition";
+  }
+
+  public ComponentI18nData getI18nData() {
+    return new ComponentI18nData(this, markerDefinition);
+  }
+  
+  public String getAttributeValueString(String attr) {
+    return getI18nData().getLocalUntranslatedValue(attr);
+  }
+
+
+  public void setAttribute(String attr, Object value) {
+    
+  }
+
+  public void add(Buildable child) {
+  }
+
+  public void addTo(Buildable parent) {
+    
+  }
+
+  public void build(Element e) {
+    
+  }
+
+  public Element getBuildElement(Document doc) {
+    return null;
+  }
+
+  public String[] getAttributeDescriptions() {
+    return null;
+  }
+
+  public Class[] getAttributeTypes() {
+    return null;
+  }
+
+  public String[] getAttributeNames() {
+    return null;
+  }
+  public Class[] getAllowableConfigureComponents() {
+    return null;
+  }
+  public HelpFile getHelpFile() {
+    return null;
+  }
+  public void removeFrom(Buildable parent) {
+    
+  }
+  
+}
