376c4958851176f63943003b4480f32972a69ef1
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2008-07-27 18:44:48 +0000

    Merged uckelman-current@3811. Broke toURL(File) out of VASSAL.build.module.documentation.HelpFile because it's used in places which have nothing to do with HelpFile.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@3895 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ src/VASSAL/tools/URLUtils.java
@@ -0,0 +1,54 @@
+/*
+ * $Id: DataArchive.java 3807 2008-07-09 23:13:30Z uckelman $
+ *
+ * Copyright (c) 2008 by Joel Uckelman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+package VASSAL.tools;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author Joel Uckelman
+ * @since 3.1.0
+ */
+public class URLUtils {
+  private URLUtils() {}
+
+  /**
+   * @param f the file for which the URL is wanted
+   * @return the URL of the file
+   * @throws MalformedURLException if the URL can't be created
+   */ 
+  public static URL toURL(File f) throws MalformedURLException {
+    String path = f.getAbsolutePath();
+
+    if (File.separatorChar != '/') {
+      path = path.replace(File.separatorChar, '/');
+    }
+
+    if (!path.startsWith("/")) { //$NON-NLS-1$
+      path = "/" + path;         //$NON-NLS-1$
+    }
+    
+    if (!path.endsWith("/") && f.isDirectory()) { //$NON-NLS-1$
+      path += "/";                                //$NON-NLS-1$
+    }
+
+    return new URL("file", "", path); //$NON-NLS-1$ //$NON-NLS-2$
+  }
+}
