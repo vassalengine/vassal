62d605d78e3639f4404a468259c0ef70fa6113d3
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2008-07-27 13:56:02 +0000

    Merged swampwallaby-work@3828. Bug 2018316: Don't move Immovable (but Selectable) units selected with Non-mova ble units by selection rectangle.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@3882 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/build/module/map/PieceMover.java
+++ src/VASSAL/build/module/map/PieceMover.java
@@ -1115,6 +1115,17 @@ public class PieceMover extends AbstractBuildable
       if (!DragBuffer.getBuffer().isEmpty()) {
         final Map map = dge.getComponent() instanceof Map.View
                       ? ((Map.View) dge.getComponent()).getMap() : null;
+                      
+        // Remove any Immovable pieces from the DragBuffer that got selected in a
+        // selection rectangle
+        List<GamePiece> pieces = new ArrayList<GamePiece>();              
+        for (PieceIterator i = DragBuffer.getBuffer().getIterator(); i.hasMoreElements(); pieces.add(i.nextPiece()));
+        for (GamePiece piece : pieces) {
+          if (Boolean.TRUE.equals(piece.getProperty(Properties.NON_MOVABLE))) {
+             DragBuffer.getBuffer().remove(piece);
+          }
+        }        
+                      
         final GamePiece piece =
           DragBuffer.getBuffer().getIterator().nextPiece();
         final Point mousePosition = map == null ?
