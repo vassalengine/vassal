85d7bd9624e7c4fb41adaf42c14732681321cf7d
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2008-10-15 13:38:35 +0000

    Added license and copyright notice.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@4253 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/tools/BugDialog.java
+++ src/VASSAL/tools/BugDialog.java
@@ -1,3 +1,21 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2008 by Joel Uckelman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
 package VASSAL.tools;
 
 import java.awt.BorderLayout;
@@ -51,7 +69,10 @@ import VASSAL.tools.swing.FlowLabel;
 import VASSAL.tools.version.VassalVersion;
 import VASSAL.tools.version.VersionUtils;
 
-
+/**
+ * @since 3.1.0
+ * @author Joel Uckelman
+ */ 
 public class BugDialog extends JDialog {
   private static final long serialVersionUID = 1L;
 
