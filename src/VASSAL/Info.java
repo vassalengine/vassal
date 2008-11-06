5e016743da9dbcf807a8ce3c11663d702ebaddf2
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2008-11-06 14:25:21 +0000

    Updated Info.version to 3.1.0-beta5. Tagged svn3495 as 3.1.0-beta5.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@4394 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/Info.java
+++ src/VASSAL/Info.java
@@ -33,7 +33,7 @@ import VASSAL.tools.version.VersionTokenizer;
  * Class for storing release-related information
  */
 public final class Info {
-  private static final String VERSION = "3.1.0-beta4"; //$NON-NLS-1$
+  private static final String VERSION = "3.1.0-beta5"; //$NON-NLS-1$
   private static File homeDir;
 
   private static final boolean isWindows;
