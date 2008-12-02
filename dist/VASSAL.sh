a13b43e86950f6b6ee7ae37b0a33e7c5b05848ab
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2008-12-02 20:18:50 +0000

    Don't use readlink flags because BSD readlink has no flags.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@4597 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- dist/VASSAL.sh
+++ dist/VASSAL.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+#
 # Execute this file to launch VASSAL on MacOS or Linux
-cd $(dirname $(readlink -f "$0")) && java -classpath lib/Vengine.jar VASSAL.launch.ModuleManager "$@"
+#
+
+# Find where VASSAL is installed, dereferencing symlinks
+INSTALL_DIR=$(dirname $(readlink "$0" || echo "$0"))
+
+# Launch VASSSAL
+cd "$INSTALL_DIR" && java -classpath lib/Vengine.jar VASSAL.launch.ModuleManager "$@"
