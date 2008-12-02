07baa0565f23fd354e1a4f7aac8da49acd9a643b
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2008-12-02 13:36:24 +0000

    Canonicalize $0 in case VASSAL.sh is being invoked via a symbolic link.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@4595 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- dist/VASSAL.sh
+++ dist/VASSAL.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Execute this file to launch VASSAL on MacOS or Linux
-cd `dirname "$0"` && java -classpath lib/Vengine.jar VASSAL.launch.ModuleManager "$@"
+cd $(dirname $(readlink -f "$0")) && java -classpath lib/Vengine.jar VASSAL.launch.ModuleManager "$@"
