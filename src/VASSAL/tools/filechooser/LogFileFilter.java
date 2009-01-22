ee8351fbd923d488fa1c6bfc4405262a17ba7524
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2009-01-22 19:38:34 +0000

    Added log file filter for use in BasicLogger.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@4955 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ src/VASSAL/tools/filechooser/LogFileFilter.java
@@ -0,0 +1,34 @@
+/*
+ * $Id: LogFileFilter.java 3937 2008-07-30 18:04:00Z uckelman $
+ *
+ * Copyright (c) 2008 by Joel Uckelman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+package VASSAL.tools.filechooser;
+
+/**
+ * A {@link FileFilter} for VASSAL logs. Used by file choosers to filter
+ * out files which aren't logs.
+ *
+ * @author Joel Uckelman
+ * @since 3.1.0
+ */
+public class LogFileFilter extends ExtensionFileFilter {
+  public static final String[] types = { ".vlog" };
+
+  public LogFileFilter() {
+    super("VASSAL Logs", types);
+  }
+}
