29b5e6dbd90f78d16a29df5e013341dc328de58a
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2009-02-01 13:13:25 +0000

    Added FIXME.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@5016 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/counters/PieceFinder.java
+++ src/VASSAL/counters/PieceFinder.java
@@ -111,9 +111,11 @@ public interface PieceFinder {
       for (Iterator<GamePiece> i = s.getPiecesInVisibleOrderIterator();
            i.hasNext();) {
         GamePiece child = i.next();
-        
+       
         // Pieces can be moved by background threads causing the size of
         // the Stack to change after the Iterator is generated. 
+        // FIXME: This is a workaround. We should fix the threading bug
+        // which causes.
         final int index = s.indexOf(child);
         if (index >= 0 && index < shapes.length) {
           if (shapes[index].contains(pt)) {
