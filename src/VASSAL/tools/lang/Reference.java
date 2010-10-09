dc99180a84fd6f9571d19bc30252fb67fe527435
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2010-10-09 11:39:24 +0000

    Merged uckelman-working@7295,7296. Added Reference class for situations where you need to pass an object to a method without keeping a reference to it.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@7320 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ src/VASSAL/tools/lang/Reference.java
@@ -0,0 +1,43 @@
+/*
+ * $Id: ImageUtils.java 6578 2010-02-28 21:20:46Z uckelman $
+ *
+ * Copyright (c) 2010 by Joel Uckelman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available 
+ * at http://www.opensource.org.
+ */
+
+package VASSAL.tools.lang;
+
+/**
+ * Holds a mutable reference.
+ *
+ * This class is useful in cases where a reference to an object must be
+ * passed to a method without the caller retaining a reference to that
+ * object itself.
+ *
+ * @since 3.2.0
+ * @author Joel Uckelman
+ */ 
+public class Reference<T> {
+  public T obj;
+
+  /**
+   * Creates a <code>Reference</code>.
+   *
+   * @param obj the referenced object
+   */
+  public Reference(T obj) {
+    this.obj = obj;
+  }
+}
