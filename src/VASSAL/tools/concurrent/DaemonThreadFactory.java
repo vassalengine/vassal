4a5387656292ecd1f4b2fcc996ace75640556f0a
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2010-10-09 12:27:23 +0000

    Added thread factory for daemon threads.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@7325 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ src/VASSAL/tools/concurrent/DaemonThreadFactory.java
@@ -0,0 +1,52 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2010 by Joel Uckelman 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+
+package VASSAL.tools.concurrent;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * A thread factory which creates daemon threads. Running daemon threads,
+ * unlike regular threads, do not prevent the application from shutting down.
+ *
+ * @since 3.2.0
+ * @author Joel Uckelman
+ */
+public class DaemonThreadFactory implements ThreadFactory {
+  protected final AtomicInteger id = new AtomicInteger(0);
+  protected final String basename; 
+
+  /**
+   * Creates a thread factory.
+   *
+   * @param basename the base name to use for threads created by this factory
+   */
+  public DaemonThreadFactory(String basename) {
+    this.basename = basename;
+  }
+
+  /** {@inheritDoc} */
+  public Thread newThread(Runnable r) {
+    final String name = basename + "-" + id.getAndIncrement();
+    final Thread t = new Thread(r, name);
+    t.setDaemon(true);
+    return t;
+  }
+}
---
+++ test/VASSAL/tools/concurrent/DaemonThreadFactoryTest.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2010 by Joel Uckelman 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+
+package VASSAL.tools.concurrent;
+
+import java.util.concurrent.ThreadFactory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DaemonThreadFactoryTest {
+  @Test
+  public void testNewThread() {
+    final String basename = "foo";
+    final ThreadFactory tf = new DaemonThreadFactory(basename);
+    
+    final Runnable r = new Runnable() {
+      public void run() {}
+    };
+
+    final Thread t0 = tf.newThread(r);
+    assertEquals(basename + "-0", t0.getName());
+    assertTrue(t0.isDaemon());
+
+    final Thread t1 = tf.newThread(r);
+    assertEquals(basename + "-1", t1.getName());
+    assertTrue(t1.isDaemon());
+  }
+}
