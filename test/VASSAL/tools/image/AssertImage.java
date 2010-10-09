2845a396f26a1ebb5ea24f0e70bd51e592bda5b5
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2010-10-09 15:21:20 +0000

    Merged uckelman-working@7291. Moved image assertions to their own class.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@7350 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ test/VASSAL/tools/image/AssertImage.java
@@ -0,0 +1,50 @@
+/*
+ * $Id: ImageIOException.java 6060 2009-10-01 18:39:12Z uckelman $
+ *
+ * Copyright (c) 2008-2009 by Joel Uckelman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+
+package VASSAL.tools.image;
+
+import java.awt.image.BufferedImage;
+
+import static org.junit.Assert.*;
+
+/**
+ * A set of assertion methods for writing tests involving images.
+ *
+ * @since 3.2.0
+ * @author Joel Uckelman
+ */ 
+public class AssertImage {
+  public static void assertImageEquals(BufferedImage expected,
+                                       BufferedImage actual) {
+    assertEquals(expected.getType(), actual.getType());
+    assertImageContentEquals(expected, actual);
+  }
+
+  public static void assertImageContentEquals(BufferedImage expected,
+                                              BufferedImage actual) {
+    assertEquals(expected.getWidth(),  actual.getWidth());
+    assertEquals(expected.getHeight(), actual.getHeight());
+
+    final int w = expected.getWidth();
+    final int h = expected.getHeight();   
+ 
+    assertArrayEquals(expected.getRGB(0, 0, w, h, null, 0, w),
+                        actual.getRGB(0, 0, w, h, null, 0, w));
+  }
+}
