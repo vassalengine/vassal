89e1de5c523e4c0a7c52f7c766a51ab8e2837c91
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2010-10-09 20:42:25 +0000

    Merged uckelman-working@7365. Added some simple listeners.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@7366 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ src/VASSAL/tools/concurrent/listener/DotPrinter.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2010 by Joel Uckelman 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+
+package VASSAL.tools.concurrent.listener;
+
+import java.io.PrintStream;
+
+/**
+ * Prints dots to a stream in response to events.
+ *
+ * @since 3.2.0
+ * @author Joel Uckelman
+ */
+public class DotPrinter<T> implements EventListener<T> {
+  protected final PrintStream out;
+
+  /**
+   * Creates a <code>DotPrinter</code>.
+   *
+   * @param out the <code>PrintStream</code> which receives the dots
+   */
+  public DotPrinter(PrintStream out) {
+    this.out = out;
+  }
+
+  /** {@inheritDoc} */
+  public void receive(Object src, T event) {
+    out.print('.');
+  }
+}
---
+++ src/VASSAL/tools/concurrent/listener/DummyEventListener.java
@@ -0,0 +1,31 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2010 by Joel Uckelman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+
+package VASSAL.tools.concurrent.listener;
+
+/**
+ * An {@link EventListener} which does nothing.
+ *
+ * @author Joel Uckelman
+ * @since 3.2.0
+ */
+public class DummyEventListener<T> implements EventListener<T> {
+  /** {@inheritDoc} */
+  public void receive(Object src, T event) {}
+}
