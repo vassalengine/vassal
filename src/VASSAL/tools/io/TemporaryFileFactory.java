84082dcd075d684687ca25f9fe9d66c7442d4695
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2010-10-09 15:37:28 +0000

    Merged uckelman-working@7276:HEAD. Oops, missed this in the previous commit.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@7352 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ src/VASSAL/tools/io/TemporaryFileFactory.java
@@ -0,0 +1,38 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2010 by Joel Uckelman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available 
+ * at http://www.opensource.org.
+ */
+package VASSAL.tools.io;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * A factory for temporary files.
+ * 
+ * @since 3.2.0
+ * @author Joel Uckelman
+ */
+public interface TemporaryFileFactory {
+  /**
+   * Creates a temporary file.
+   *
+   * @return a temporary file
+   * @throws IOException if the file cannot be created
+   */
+  public File create() throws IOException;
+}
