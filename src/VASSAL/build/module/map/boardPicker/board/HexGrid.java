4884b6299d31280defe9c928b8514add0bf22b3f
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2012-03-26 01:33:20 +0000

    Added (deprecated) methods back to HexGrid to accomodate bringing modules with custom code (e.g., VASL) up to 3.2 compatibility.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@8103 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/build/module/map/boardPicker/board/HexGrid.java
+++ src/VASSAL/build/module/map/boardPicker/board/HexGrid.java
@@ -19,13 +19,83 @@
 
 package VASSAL.build.module.map.boardPicker.board;
 
-/** Dummy wrapper for {@link HexGridX} to facilitate synchronous
+import java.awt.Point;
+
+/**
+ * Dummy wrapper for {@link HexGridX} to facilitate synchronous
  * parallel development.
  * @author Pieter Geerkens
- *
  */
 public class HexGrid extends HexGridX {
   public HexGrid() { super(); }
+
   public HexGrid(double size) { super(size); }
+
   public HexGrid(double width, double height) { super(width, height); }
+
+  @Deprecated
+  public HexGrid(double size, boolean dummyForASL) { super(size); }
+
+  @Deprecated
+  public double getHexSize() {
+    return getDy();
+  }
+
+  @Deprecated
+  public double getHexWidth() {
+    return getDx();
+  }
+
+  @Deprecated
+  protected boolean alternate = false;
+
+  @Deprecated
+  protected double dx = getDx();
+
+  @Deprecated
+  protected double dy = getDy();
+
+  @Deprecated
+  protected Point origin = new Point(0, 32);
+
+  @Deprecated
+  public void setSnapScale(int value) {
+    // dummy method
+  }
+
+  @Deprecated
+  public void setHexWidth(double value) {
+    setDx(value);
+  }
+
+  @Deprecated
+  protected int sideX(int x, int y) {
+    return ((int) (dx / 2 * (int) Math.floor((x - origin.x + dx / 4) * 2 / dx) + origin.x));
+  }
+
+  @Deprecated
+  protected int sideY(int x, int y) {
+    final int nx = (int) Math.floor((x - origin.x + dx / 4) * 2 / dx);
+    if (nx % 2 == 0) {
+      return ((int) (dy / 2 * (int) Math.floor((y - origin.y + dy / 4) * 2 / dy) + origin.y));
+    }
+    else {
+      return ((int) ((dy / 2) * (int) Math.floor((y - origin.y) * 2 / dy) + (int) (dy / 4) + origin.y));
+    }
+  }
+
+  @Deprecated
+  public Point snapToHexVertex(Point p) {
+    return super.snapToHexVertex(p);
+  }
+
+  @Deprecated
+  public void setCornersLegal(boolean cornersLegal_) {
+    cornersLegal = cornersLegal_;
+  }
+
+  @Deprecated
+  public void setEdgesLegal(boolean edgesLegal_) {
+    edgesLegal = edgesLegal_;
+  }
 }
