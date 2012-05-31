aeb30cf67643070ebc9f20b0a5fd0d4b848ca1f1
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2012-05-31 21:35:00 +0000

    Ack, missed the correct revision.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@8192 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/tools/version/VassalVersionTokenizer.java
+++ src/VASSAL/tools/version/VassalVersionTokenizer.java
@@ -68,7 +68,7 @@ public class VassalVersionTokenizer implements VersionTokenizer {
   // as soon as 3.1.1 is released.) We keep one tag for testing purposes.
   static {
     // 3.2.0
-    tags.put("beta1", 8191);
+    tags.put("beta1", 8193);
   }
 
   /**
