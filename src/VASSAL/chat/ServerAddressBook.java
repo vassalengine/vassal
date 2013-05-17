340e1f18a19bf3e34bd64b810448fc66514d1671
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2013-05-17 20:31:36 +0000

    Merged Brent-3.2-p2p@8706.
    Fixed Bug 10170: Incorrect LAN address reported under Linux
    
    
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@8709 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/chat/ServerAddressBook.java
+++ src/VASSAL/chat/ServerAddressBook.java
@@ -28,6 +28,7 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.UnknownHostException;
 import java.util.Enumeration;
 import java.util.List;
@@ -77,6 +78,7 @@ public class ServerAddressBook {
   protected static final String P2P_MODE_KEY = P2PClientFactory.P2P_MODE_KEY;
   protected static final String P2P_SERVER_MODE = P2PClientFactory.P2P_SERVER_MODE;
   protected static final String P2P_CLIENT_MODE = P2PClientFactory.P2P_CLIENT_MODE;
+  protected static final String PRIVATE_TYPE = PrivateClientFactory.PRIVATE_TYPE;
   protected static final String TYPE_KEY = ChatServerFactory.TYPE_KEY;
   protected static final String DESCRIPTION_KEY = "description"; //$NON-NLS-1$
   protected final int LEAF_ICON_SIZE = IconFamily.SMALL;
@@ -114,7 +116,7 @@ public class ServerAddressBook {
   public static String getLocalAddress() {
     if (localIPAddress == null) {
       try {
-        localIPAddress = InetAddress.getLocalHost().getHostAddress();
+        localIPAddress = getLocalHostLANAddress().getHostAddress();
       }
       catch (UnknownHostException e) {
         localIPAddress = "?"; //$NON-NLS-1$
@@ -153,6 +155,77 @@ public class ServerAddressBook {
     return theIp;
   }
 
+  /**
+   * Returns an <code>InetAddress</code> object encapsulating what is most likely the machine's LAN IP address.
+   * <p/>
+   * This method is intended for use as a replacement of JDK method <code>InetAddress.getLocalHost</code>, because
+   * that method is ambiguous on Linux systems. Linux systems enumerate the loopback network interface the same
+   * way as regular LAN network interfaces, but the JDK <code>InetAddress.getLocalHost</code> method does not
+   * specify the algorithm used to select the address returned under such circumstances, and will often return the
+   * loopback address, which is not valid for network communication. Details
+   * <a href="http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4665037">here</a>.
+   * <p/>
+   * This method will scan all IP addresses on all network interfaces on the host machine to determine the IP address
+   * most likely to be the machine's LAN address. If the machine has multiple IP addresses, this method will prefer
+   * a site-local IP address (e.g. 192.168.x.x or 10.10.x.x, usually IPv4) if the machine has one (and will return the
+   * first site-local address if the machine has more than one), but if the machine does not hold a site-local
+   * address, this method will return simply the first non-loopback address found (IPv4 or IPv6).
+   * <p/>
+   * If this method cannot find a non-loopback address using this selection algorithm, it will fall back to
+   * calling and returning the result of JDK method <code>InetAddress.getLocalHost</code>.
+   * <p/>
+   *
+   * @throws UnknownHostException If the LAN address of the machine cannot be found.
+   * 
+   * Thanks to https://issues.apache.org/jira/browse/JCS-40 for this code
+   */
+  private static InetAddress getLocalHostLANAddress() throws UnknownHostException {
+      try {
+          InetAddress candidateAddress = null;
+          // Iterate all NICs (network interface cards)...
+          for (Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
+              NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+              // Iterate all IP addresses assigned to each card...
+              for (Enumeration<InetAddress> inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
+                  InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+                  if (!inetAddr.isLoopbackAddress()) {
+
+                      if (inetAddr.isSiteLocalAddress()) {
+                          // Found non-loopback site-local address. Return it immediately...
+                          return inetAddr;
+                      }
+                      else if (candidateAddress == null) {
+                          // Found non-loopback address, but not necessarily site-local.
+                          // Store it as a candidate to be returned if site-local address is not subsequently found...
+                          candidateAddress = inetAddr;
+                          // Note that we don't repeatedly assign non-loopback non-site-local addresses as candidates,
+                          // only the first. For subsequent iterations, candidate will be non-null.
+                      }
+                  }
+              }
+          }
+          if (candidateAddress != null) {
+              // We did not find a site-local address, but we found some other non-loopback address.
+              // Server might have a non-site-local address assigned to its NIC (or it might be running
+              // IPv6 which deprecates the "site-local" concept).
+              // Return this non-loopback candidate address...
+              return candidateAddress;
+          }
+          // At this point, we did not find a non-loopback address.
+          // Fall back to returning whatever InetAddress.getLocalHost() returns...
+          InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+          if (jdkSuppliedAddress == null) {
+              throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+          }
+          return jdkSuppliedAddress;
+      }
+      catch (Exception e) {
+          UnknownHostException unknownHostException = new UnknownHostException("Failed to determine LAN address: " + e);
+          unknownHostException.initCause(e);
+          throw unknownHostException;
+      }
+  }
+  
   public ServerAddressBook() {
     instance = this;
   }
@@ -407,7 +480,13 @@ public class ServerAddressBook {
       public void actionPerformed(ActionEvent arg0) {
         addEntry(new JabberEntry());        
       }});
+    final JMenuItem privateItem = new JMenuItem(Resources.getString("ServerAddressBook.private_server"));
+    privateItem.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent arg0) {
+        addEntry(new PrivateEntry());        
+      }});
     popup.add(p2pItem);
+    popup.add(privateItem);
     popup.add(jabItem);
     popup.show(addButton, 0, 0);
   }
@@ -949,6 +1028,73 @@ public class ServerAddressBook {
 
   }
 
+  /**
+   * Address Book entry for a Private VASSAL server
+   *
+   */
+  private class PrivateEntry extends AddressBookEntry {
+    
+    private JTextField serverPort = new JTextField();
+    private JTextField serverIp = new JTextField();
+    
+    public PrivateEntry() {
+      this(new Properties());
+      setDescription(Resources.getString("ServerAddressBook.private_server")); //$NON-NLS-1$
+      setType(PRIVATE_TYPE);
+      setProperty(PrivateClientFactory.PORT, "5050"); //$NON-NLS-1$
+    }
+
+    public PrivateEntry(Properties props) {
+      super(props);
+    }
+
+    public String toString() {
+      return Resources.getString("ServerAddressBook.private_server") + " [" + getDescription() + "]";
+    }
+    
+    public String getDescription() {
+      return super.getDescription() + " " + getProperty(PrivateClientFactory.URL) + ":" + getProperty(PrivateClientFactory.PORT);
+    }
+
+    protected String getIconName() {
+      return "VASSAL-private"; //$NON-NLS-1$
+    }
+
+    protected boolean isRemovable() {
+      return true;
+    }
+
+    protected boolean isEditable() {
+      return true;
+    }
+
+    protected boolean isDescriptionEditable() {
+      return true;
+    }
+
+    protected void addAdditionalControls(JComponent c, boolean enabled) {
+      serverIp.setEditable(enabled);
+      c.add(new JLabel(Resources.getString("ServerAddressBook.server_ip"))); //$NON-NLS-1$
+      c.add(serverIp, "wrap, growx, push"); //$NON-NLS-1$
+      
+      serverPort.setEditable(enabled);
+      c.add(new JLabel(Resources.getString("ServerAddressBook.server_port"))); //$NON-NLS-1$
+      c.add(serverPort, "wrap, growx, push"); //$NON-NLS-1$
+    }
+
+    protected void getAdditionalProperties(Properties props) {
+      props.setProperty(PrivateClientFactory.URL, serverIp.getText());
+      props.setProperty(PrivateClientFactory.PORT, serverPort.getText());
+      props.setProperty(TYPE_KEY, PRIVATE_TYPE);
+    }
+
+    protected void setAdditionalProperties(Properties props) {
+      serverIp.setText(props.getProperty(PrivateClientFactory.URL));
+      serverPort.setText(props.getProperty(PrivateClientFactory.PORT));
+    }
+
+  }
+  
   /**
    * Address Book Entry for a Peer to Peer connection 
    *
