2461f318ef8b7b8f8f290297733195a072326238
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2013-09-10 18:19:08 +0000

    Fixed Bug 10468: VASSAL.sh doesn't work if path includes spaces
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@8824 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- dist/VASSAL.sh
+++ dist/VASSAL.sh
@@ -5,7 +5,7 @@
 #
 
 # Find where VASSAL is installed, dereferencing symlinks
-INSTALL_DIR=$(dirname $(readlink "$0" || echo "$0"))
+INSTALL_DIR=$(dirname "$(readlink "$0" || echo "$0")")
 
 # Launch VASSSAL
 cd "$INSTALL_DIR" && java -classpath lib/Vengine.jar VASSAL.launch.ModuleManager "$@"
