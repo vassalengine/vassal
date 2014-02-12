adb630a8ece1d7ef2886fa972ed285c7911b0236
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2014-02-12 09:35:51 +0000

    Simplified check for Apple's Java 5/6.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@9007 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- dist/macosx/VASSAL.sh
+++ dist/macosx/VASSAL.sh
@@ -5,22 +5,18 @@ APP_ROOT="$(dirname "$0")/../.."
 cd "$APP_ROOT"
 
 # find a JRE to use
-# try Java 6
-JAVA="/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Commands/java"
+# try Apple's Java
+JAVA="/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/java"
 if [ ! -x "$JAVA" ]; then
-  # try Java 7
+  # try Oracle's Java 7
   JAVA="/Library/Internet Plug-ins/JavaAppletPlugin.plugin/Contents/Home/bin/java"
   if [ ! -x "$JAVA" ]; then
-    # try Java 5
-    JAVA="/System/Library/Frameworks/JavaVM.framework/Versions/1.5/Commands/java"
+    # try whatever's on the PATH as a last resort
+    JAVA="$(which java)"
     if [ ! -x "$JAVA" ]; then
-      # try whatever's on the PATH
-      JAVA="$(which java)"
-      if [ ! -x "$JAVA" ]; then
-        # you seem not to have Java
-        osascript -e 'tell app "System Events" to display alert "VASSAL requires Java in order to run. Please install Java before starting VASSAL." as critical buttons {"OK"}'
-        exit 1
-      fi
+      # you seem not to have Java
+      osascript -e 'tell app "System Events" to display alert "VASSAL requires Java in order to run. Please install Java before starting VASSAL." as critical buttons {"OK"}'
+      exit 1
     fi
   fi
 fi
