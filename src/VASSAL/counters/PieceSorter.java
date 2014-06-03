2e6d437a451306c8e8a59c351fd09ecfe0ff2e73
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2014-06-03 13:10:59 +0000

    Fixed Bug 10925: IllegalArgumentException due to intransitive Comparator in KeyBuffer.sort()
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@9045 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/counters/PieceSorter.java
+++ src/VASSAL/counters/PieceSorter.java
@@ -28,30 +28,32 @@ import java.util.Comparator;
  */
 public class PieceSorter implements Comparator<GamePiece> {
   public int compare(GamePiece p1, GamePiece p2) {
-    if (p1.getMap() == null && p2.getMap() == null) {
-      return 0;
-    }
-    else if (p1.getMap() == null) {
-      return 1;
+    if (p1.getMap() == null) {
+      return p2.getMap() == null ? 0 : 1;
     }
     else if (p2.getMap() == null) {
       return -1;
     }
 
-    int result = 0;
     if (p1.getMap() != p2.getMap()) {
-      result = p1.getMap().getId().compareTo(p2.getMap().getId());
+      return p1.getMap().getId().compareTo(p2.getMap().getId());
+    }
+
+    final Stack s1 = p1 instanceof Stack ? (Stack) p1 : p1.getParent();
+    final Stack s2 = p2 instanceof Stack ? (Stack) p2 : p2.getParent();
+
+    if (s1 == null) {
+      return s2 == null ? 0 : 1;
+    }
+    else if (s2 == null) {
+      return -1;
     }
     else {
-      Stack s1 = p1 instanceof Stack ? (Stack) p1 : p1.getParent();
-      Stack s2 = p2 instanceof Stack ? (Stack) p2 : p2.getParent();
-      if (s1 != null && s2 != null) {
-        result = p1.getMap().indexOf(s1) - p2.getMap().indexOf(s2);
-        if (result == 0) { // Pieces must be in the same stack
-          result = s1.indexOf(p1) - s2.indexOf(p2);
-        }
+      int result = p1.getMap().indexOf(s1) - p2.getMap().indexOf(s2);
+      if (result == 0) { // Pieces must be in the same stack
+        result = s1.indexOf(p1) - s2.indexOf(p2);
       }
+      return result;
     }
-    return result;
   }
 }
