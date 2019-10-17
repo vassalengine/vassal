b500858e1ecfde075f3d2510418c2c91a0c9b17c
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2019-10-17 22:38:20 +0000

    Don't hunt for obsolete versions of Java on OS X.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@9280 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- dist/macosx/VASSAL.sh
+++ dist/macosx/VASSAL.sh
@@ -4,27 +4,6 @@
 APP_ROOT="$(dirname "$0")/../.."
 cd "$APP_ROOT"
 
-# find Java
-# try Apple's Java 6
-JAVA="/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Commands/java"
-if [ ! -x "$JAVA" ]; then
-  # try Apple's Java 5
-  JAVA="/System/Library/Frameworks/JavaVM.framework/Versions/1.5/Commands/java"
-  if [ ! -x "$JAVA" ]; then
-    # try Oracle's Java 7
-    JAVA="/Library/Internet Plug-ins/JavaAppletPlugin.plugin/Contents/Home/bin/java"
-    if [ ! -x "$JAVA" ]; then
-      # try whatever's on the PATH as a last resort
-      JAVA="$(which java)"
-      if [ ! -x "$JAVA" ]; then
-        # you seem not to have Java
-        osascript -e 'tell app "System Events" to display alert "VASSAL requires Java in order to run. Please install Java before starting VASSAL." as critical buttons {"OK"}'
-        exit 1
-      fi
-    fi
-  fi
-fi
-
 # filter out the -psn_* option added by Finder
 ARGS=()
 for ARG in "$@"; do
@@ -34,4 +13,4 @@ for ARG in "$@"; do
 done
 
 # fire it up
-exec "$JAVA" -classpath Contents/Resources/Java/Vengine.jar -Dapple.awt.graphics.UseQuartz=false -Xdock:name=VASSAL -Xdock:icon=Contents/Resources/VASSAL.icns VASSAL.launch.ModuleManager "${ARGS[@]}"
+exec java -classpath Contents/Resources/Java/Vengine.jar -Dapple.awt.graphics.UseQuartz=false -Xdock:name=VASSAL -Xdock:icon=Contents/Resources/VASSAL.icns VASSAL.launch.ModuleManager "${ARGS[@]}"
