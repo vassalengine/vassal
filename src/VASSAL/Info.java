92515ce86492fbea3394d7038ffc7a9bf609ec5f
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2020-03-03 20:49:51 +0000

    Increment the expiry version.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@9294 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/Info.java
+++ src/VASSAL/Info.java
@@ -38,7 +38,7 @@ public final class Info {
   private static final String VERSION = "3.2.16"; //$NON-NLS-1$
 
   // Do not allow editing of modules with this revision or later
-  private static final String EXPIRY_VERSION = "3.3";  //$NON-NLS-1$
+  private static final String EXPIRY_VERSION = "3.4";  //$NON-NLS-1$
 
   // Warn about editing modules, saves, logs written before this version
   private static final String UPDATE_VERSION =  "3.2";
