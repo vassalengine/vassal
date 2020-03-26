172d12b7485bc8296decf362b8e0f986339caf92
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2020-03-26 22:18:21 +0000

    Cleanup.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/trunk@9305 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/build/module/map/MenuDisplayer.java
+++ src/VASSAL/build/module/map/MenuDisplayer.java
@@ -204,8 +204,7 @@ public class MenuDisplayer extends MouseAdapter implements Buildable {
       final GamePiece p = map.findPiece(e.getPoint(), targetSelector);
       if (p != null) {
         EventFilter filter = (EventFilter) p.getProperty(Properties.SELECT_EVENT_FILTER);
-        if (filter == null
-            || !filter.rejectEvent(e)) {
+        if (filter == null || !filter.rejectEvent(e)) {
           JPopupMenu popup = createPopup(p, true);
           Point pt = map.componentCoordinates(e.getPoint());
           popup.addPopupMenuListener(new javax.swing.event.PopupMenuListener() {
