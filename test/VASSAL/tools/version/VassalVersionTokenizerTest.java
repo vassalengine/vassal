0737e567151a765d07ff79afe26852efece4bb11
Joel Uckelman <uckelman@nomic.net>
2020-06-11 00:32:21 +0100

    Bumped version to 3.3.1. Removed old svn version parsing code since it's needed only for parsing versions < 3.3.0.

--- test/VASSAL/tools/version/VassalVersionTokenizerTest.java
+++ test/VASSAL/tools/version/VassalVersionTokenizerTest.java
@@ -30,16 +30,17 @@ public class VassalVersionTokenizerTest {
     // expected tokens. A null indicates that a VersionFormatException
     // is expected.
     final Object[][] versions = {
-      { "1.2.3",                    1, 2, 3               },
-      { "1.2.3.4",                  1, 2, 3, 4            },
-      { "1.2.3-rc3",                1, 2, 3, -2, null     },
-      { "foobarbaz",                null                  },
-      { "1.2.foo",                  1, 2, null            },
-      { "1.2-foo",                  1, 2, -2, null        },
-      { "3.0b6",                    3, 0, null            },
-      { "3.3.0-beta4",              3, 3, 0, -2, 9453     },
-      { "3.3.0-beta4-80",           3, 3, 0, -2, 9453, 80 },
-      { "3.3.0-beta4-80-gf8ef2523", 3, 3, 0, -2, 9453, 80 }
+      { "1.2.3",                   1, 2, 3            },
+      { "1.2.3.4",                 1, 2, 3, 4         },
+      { "1.2.3-svn7890",           1, 2, 3, -2, null  },
+      { "1.2.3-rc3",               1, 2, 3, -2, null  },
+      { "foobarbaz",               null               },
+      { "1.2.foo",                 1, 2, null         },
+      { "1.2-foo",                 1, 2, -2, null     },
+      { "3.0b6",                   3, 0, null         },
+      { "3.3.1-test",              3, 3, 1, -2, 0     },
+      { "3.3.1-test-80",           3, 3, 1, -2, 0, 80 },
+      { "3.3.1-test-80-gf8ef2523", 3, 3, 1, -2, 0, 80 }
     };
 
     for (Object[] v : versions) {
