62c2bb8459d330a1f37dde67a3959264ee577400
Cattlesquat <brian@brianreynolds.net>
2020-07-06 14:19:15 -0400

    Cleanup clutter. Console hook.

--- src/main/java/VASSAL/build/module/Chatter.java
+++ src/main/java/VASSAL/build/module/Chatter.java
@@ -72,13 +72,13 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
   protected JTextField input;
   protected JScrollPane scroll = new ScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
   protected JScrollPane scroll2 = new ScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-  protected static final String MY_CHAT_COLOR    = "HTMLChatColor";          //$NON-NLS-1$ //BR// Different tags to "restart" w/ new default scheme
+  protected static final String MY_CHAT_COLOR    = "HTMLChatColor";          //$NON-NLS-1$ // Different tags to "restart" w/ new default scheme
   protected static final String OTHER_CHAT_COLOR = "HTMLotherChatColor";     //$NON-NLS-1$
   protected static final String GAME_MSG1_COLOR  = "HTMLgameMessage1Color";  //$NON-NLS-1$
-  protected static final String GAME_MSG2_COLOR  = "HTMLgameMessage2Color";  //$NON-NLS-1$ //BR//
-  protected static final String GAME_MSG3_COLOR  = "HTMLgameMessage3Color";  //$NON-NLS-1$ //BR//
-  protected static final String GAME_MSG4_COLOR  = "HTMLgameMessage4Color";  //$NON-NLS-1$ //BR//
-  protected static final String GAME_MSG5_COLOR  = "HTMLgameMessage5Color";  //$NON-NLS-1$ //BR//
+  protected static final String GAME_MSG2_COLOR  = "HTMLgameMessage2Color";  //$NON-NLS-1$ 
+  protected static final String GAME_MSG3_COLOR  = "HTMLgameMessage3Color";  //$NON-NLS-1$ 
+  protected static final String GAME_MSG4_COLOR  = "HTMLgameMessage4Color";  //$NON-NLS-1$ 
+  protected static final String GAME_MSG5_COLOR  = "HTMLgameMessage5Color";  //$NON-NLS-1$ 
   protected static final String SYS_MSG_COLOR    = "HTMLsystemMessageColor"; //$NON-NLS-1$
   protected static final String HTML_COMPATIBILITY = "HTMLcompatibility";    //$NON-NLS-1$
 
@@ -87,7 +87,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
   protected Color gameMsg, gameMsg2, gameMsg3, gameMsg4, gameMsg5;
   protected Color systemMsg, myChat, otherChat;
 
-  protected boolean htmlCompatibility; //BR// Disable HTML parsing unless opted in, to maintain backwards compatibility
+  protected boolean htmlCompatibility; // Disable HTML parsing unless opted in, to maintain backwards compatibility
 
   public static String getAnonymousUserName() {
     return Resources.getString("Chat.anonymous"); //$NON-NLS-1$
@@ -103,7 +103,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
     kit = (HTMLEditorKit) conversation.getEditorKit();
 
     style = kit.getStyleSheet();
-    myFont = new Font("SansSerif", Font.PLAIN, 12); //BR// Will be overridden by the font from Chat preferences
+    myFont = new Font("SansSerif", Font.PLAIN, 12); // Will be overridden by the font from Chat preferences
 
     for (int i = 0; i < 15; ++i) {
       try {
@@ -142,7 +142,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
     conversation.setPreferredSize(new Dimension(input.getMaximumSize().width, fontHeight * 10));
 
     scroll.setViewportView(conversation);
-    scroll.getVerticalScrollBar().setUnitIncrement(input.getPreferredSize().height); // BR// Scroll this faster																									
+    scroll.getVerticalScrollBar().setUnitIncrement(input.getPreferredSize().height); //Scroll this faster																									
     add(scroll);
     add(input);
     
@@ -151,15 +151,15 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
 
   private String formatChat(String text) {
     final String id = GlobalOptions.getInstance().getPlayerId();
-    return String.format("&lt;%s&gt; - %s", id.isEmpty() ? "(" + getAnonymousUserName() + ")" : id, text); //BR//HTML-friendly angle brackets //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    return String.format("&lt;%s&gt; - %s", id.isEmpty() ? "(" + getAnonymousUserName() + ")" : id, text); //HTML-friendly angle brackets //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
   }
 
   public JTextField getInputField() {
     return input;
   }
 
-  //BR// To make the player colors easy to override in a custom class 
-  //BR// (my modules have logic to assign individual player colors -- beyond the scope of the present effort but a perhaps a fun future addition)
+  // To make the player colors easy to override in a custom class 
+  // (my modules have logic to assign individual player colors -- beyond the scope of the present effort but a perhaps a fun future addition)
   protected String getChatStyle(String s)
   {   
     if (s.startsWith(formatChat("").trim())) { //$NON-NLS-1$
@@ -169,6 +169,12 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
       return "other";
     }   
   }
+    
+  // A hook for inserting a console class that accepts commands 
+  protected void consoleHook(String s, String style, boolean html_allowed)
+  {
+    
+  }
 
   /**
    * Display a message in the text area
@@ -177,18 +183,18 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
     String style;
     boolean html_allowed;
 
-    //BR// Choose an appropriate style to display this message in
+    // Choose an appropriate style to display this message in
     s = s.trim();
     if (!s.isEmpty()) {
       if (s.startsWith("*")) {
-        //BR// Here we just extend the convention of looking at first characters, this time to the second character.
-        //BR// | = msg1 (w/ HTML parsing explicitly opted in)
-        //BR// ! = msg2
-        //BR// ? = msg3
-        //BR// ~ = msg4
-        //BR// ` = msg5
-        //BR// These characters can be pre-pended to Report messages to produce the color changes. The characters themselves are removed before display.
-        //BR// Reports can also include <b></b> tags for bold and <i></i> for italic.
+        // Here we just extend the convention of looking at first characters, this time to the second character.
+        // | = msg1 (w/ HTML parsing explicitly opted in)
+        // ! = msg2
+        // ? = msg3
+        // ~ = msg4
+        // ` = msg5
+        // These characters can be pre-pended to Report messages to produce the color changes. The characters themselves are removed before display.
+        // Reports can also include <b></b> tags for bold and <i></i> for italic.
         if (s.startsWith("* |") || s.startsWith("*|")) {
           style = "msg";
           s = s.replaceFirst("\\|", "");
@@ -216,7 +222,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
         } 
         else {
           style = "msg";
-          html_allowed = !htmlCompatibility; //BR// Generic report lines check compatibility flag (so old modules will not break on e.g. "<" in messages)
+          html_allowed = !htmlCompatibility; // Generic report lines check compatibility flag (so old modules will not break on e.g. "<" in messages)
         }
       } 
       else if (s.startsWith("-")) {
@@ -233,20 +239,17 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
       html_allowed = false;
     }
 
-    //BR// Disable unwanted HTML tags in contexts where it shouldn't be allowed:
-    //BR// (1) Anything received from chat channel, for security reasons
-    //BR// (2) Legacy module "report" text when not explicitly opted in w/ first character or preference setting 
+    // Disable unwanted HTML tags in contexts where it shouldn't be allowed:
+    // (1) Anything received from chat channel, for security reasons
+    // (2) Legacy module "report" text when not explicitly opted in w/ first character or preference setting 
     if (!html_allowed) {
-      s = s.replaceAll("<", "&lt;") //BR// This prevents any unwanted tag from functioning
-           .replaceAll(">", "&gt;"); //BR// This makes sure > doesn't break any of our legit <div> tags
+      s = s.replaceAll("<", "&lt;")  // This prevents any unwanted tag from functioning
+           .replaceAll(">", "&gt;"); // This makes sure > doesn't break any of our legit <div> tags
     }
 
-    //BR// Systematically search through for html image tags. When we find one, try
-    //BR// to match it with an image from our DataArchive, and substitute the correct
-    //BR// fully qualified URL into the tag.
-
-    //BR// There are without a doubt "more efficient ways to do this", but this loop
-    //BR// miraculously ran perfectly the very first time I compiled it, so superstition...		
+    // Systematically search through for html image tags. When we find one, try
+    // to match it with an image from our DataArchive, and substitute the correct
+    // fully qualified URL into the tag.
     URL url;
     String keystring = "<img src=\"";
     String file, tag, replace;
@@ -270,20 +273,20 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
         s = s.replaceFirst(tag, replace); // Swap in our new URL-laden tag for the old one.
       } 
       else {
-        break; // BR// If something went wrong in matching up the tag, don't loop forever
+        break; // If something went wrong in matching up the tag, don't loop forever
       }
     }
 
-    //BR// Now we have to fix up any legacy angle brackets around the word <observer>
+    // Now we have to fix up any legacy angle brackets around the word <observer>
     keystring = Resources.getString("PlayerRoster.observer");
     replace = keystring.replace("<","&lt;").replace(">","&gt;");
     if (replace != keystring) {
       s = s.replace(keystring, replace);
     }
 
-    //BR// Insert a div of the correct style for our line of text. Module designer
-    //BR//still free to insert <span> tags and <img> tags and the like in Report
-    //BR//messages.
+    // Insert a div of the correct style for our line of text. Module designer
+    // still free to insert <span> tags and <img> tags and the like in Report
+    // messages.
     try {
       kit.insertHTML(doc, doc.getLength(), "\n<div class=" + style + ">" + s + "</div>", 0, 0, null);
     } 
@@ -293,7 +296,8 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
     catch (IOException ex) {
       ErrorDialog.bug(ex);
     }
-    conversation.update(conversation.getGraphics()); //BR// Force graphics to update
+    conversation.update(conversation.getGraphics()); // Force graphics to update
+    consoleHook(s, style, html_allowed);             
   }
 
   /** @deprecated use GlobalOptions.getPlayerId() */
@@ -307,7 +311,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
     return GlobalOptions.getInstance().getPlayerId();
   }
 
-  //BR// Adds or updates a CSS stylesheet entry. Styles in the color, font type, and font size.
+  // Adds or updates a CSS stylesheet entry. Styles in the color, font type, and font size.
   private void addStyle(String s, Font f, Color c, String font_weight, int size) {
     if ((style == null) || (c == null)) return;
     style.addRule(s + 
@@ -322,7 +326,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
                   "}");
   }
 
-  //BR// Build ourselves a CSS stylesheet from our preference font/color settings.
+  // Build ourselves a CSS stylesheet from our preference font/color settings.
   private void makeStyleSheet(Font f) {
     if (style == null) {
       return;
@@ -348,7 +352,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
     addStyle(".other ", f, otherChat, "bold", 0);
     addStyle(".sys",    f, systemMsg, "",     0);
 
-    //BR// A fun extension would be letting the module designer provide extra class styles. 
+    // A fun extension would be letting the module designer provide extra class styles. 
   }
 
   /**
@@ -370,7 +374,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
     if (conversation != null) {
       conversation.setFont(f);
     }
-    makeStyleSheet(f); // BR// When font changes, rebuild our stylesheet
+    makeStyleSheet(f); // When font changes, rebuild our stylesheet
   }
 
   @Override
@@ -389,7 +393,6 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
   @Override
   public void addTo(Buildable b) {
     GameModule mod = (GameModule) b;
-    mod.removeCommandEncoder(mod.getChatter()); //BR//
     mod.setChatter(this);
     mod.addCommandEncoder(this);
     mod.addKeyStrokeSource(new KeyStrokeSource(this, WHEN_ANCESTOR_OF_FOCUSED_COMPONENT));
@@ -431,9 +434,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
 
     gameMsg = (Color) globalPrefs.getValue(GAME_MSG1_COLOR);
 
-    //BR//
-    //BR// game message color #2 (messages starting with "!") 
-    //BR//
+    // game message color #2 (messages starting with "!") 
     final ColorConfigurer gameMsg2Color = new ColorConfigurer(GAME_MSG2_COLOR,
                                                               Resources.getString("Chatter.game_messages_preference") + "(#2 - first character \"!\"):  ", new Color(0, 153, 51));
 
@@ -449,9 +450,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
 
     gameMsg2 = (Color) globalPrefs.getValue(GAME_MSG2_COLOR);
 
-    //BR//
-    //BR// game message color #3 (messages starting with "?")
-    //BR//
+    // game message color #3 (messages starting with "?")
     final ColorConfigurer gameMsg3Color = new ColorConfigurer(GAME_MSG3_COLOR,
                                                               Resources.getString("Chatter.game_messages_preference") + "(#3 - first character \"?\"):  ", new Color(255, 102, 102));
 
@@ -468,9 +467,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
     gameMsg3 = (Color) globalPrefs.getValue(GAME_MSG3_COLOR);
 
 
-    //BR//
-    //BR// game message color #4 (messages starting with "~")
-    //BR//
+    // game message color #4 (messages starting with "~")
     final ColorConfigurer gameMsg4Color = new ColorConfigurer(GAME_MSG4_COLOR,
                                                               Resources.getString("Chatter.game_messages_preference") + "(#4 - first character \"~\"):  ", new Color(255, 0, 0));
 
@@ -487,9 +484,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
     gameMsg4 = (Color) globalPrefs.getValue(GAME_MSG4_COLOR);
 
 
-    //BR//
-    //BR// game message color #5 (messages starting with "`")
-    //BR//
+    // game message color #5 (messages starting with "`")
     final ColorConfigurer gameMsg5Color = new ColorConfigurer(GAME_MSG5_COLOR,
                                                               Resources.getString("Chatter.game_messages_preference") + "(#5 - first character \"`\"):  ", new Color(153, 0, 153));
 
@@ -643,7 +638,7 @@ public class Chatter extends JPanel implements CommandEncoder, Buildable {
       this.c = c;
       msg = s;
       if (msg.startsWith("<>")) {
-        msg = "&lt;(" + Chatter.getAnonymousUserName() + ")&gt;" + s.substring(2); // BR// HTML-friendly
+        msg = "&lt;(" + Chatter.getAnonymousUserName() + ")&gt;" + s.substring(2); // HTML-friendly
         // angle brackets
       } 
       else {
