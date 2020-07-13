cf75b98096b6bc4db86f313e8f3b7b03c40d040b
Joel Uckelman <uckelman@nomic.net>
2020-07-13 20:46:41 +0100

    Whitespace.

--- src/VASSAL/build/module/map/boardPicker/board/mapgrid/PolygonEditor.java
+++ src/VASSAL/build/module/map/boardPicker/board/mapgrid/PolygonEditor.java
@@ -227,88 +227,86 @@ public class PolygonEditor extends JPanel {
 
     @Override
     public void mousePressed(MouseEvent e) {
-       selected = -1;
-       double minDist = Float.MAX_VALUE;
+      selected = -1;
+      double minDist = Float.MAX_VALUE;
 
-       if (SwingUtils.isLeftMouseButton(e)) {
-         // move an existing vertex
-               for (int i = 0; i < polygon.npoints; ++i) {
-                       double dist = Point2D.distance(polygon.xpoints[i], polygon.ypoints[i], e.getX(), e.getY());
-                       if (dist < minDist) {
-                               minDist = dist;
-                               selected = i;
-                       }
-               }
-       } 
-       else if (SwingUtils.isRightMouseButton(e)) {
-               // find closest segment/vertex
-               selected = -1;
-               minDist = Float.MAX_VALUE;
-               boolean isVertex = false;
+      if (SwingUtils.isLeftMouseButton(e)) {
+        // move an existing vertex
+        for (int i = 0; i < polygon.npoints; ++i) {
+        double dist = Point2D.distance(polygon.xpoints[i], polygon.ypoints[i], e.getX(), e.getY());
+          if (dist < minDist) {
+            minDist = dist;
+            selected = i;
+          }
+        }
+      } 
+      else if (SwingUtils.isRightMouseButton(e)) {
+        // find closest segment/vertex
+        selected = -1;
+        minDist = Float.MAX_VALUE;
+        boolean isVertex = false;
 
-               int x0 = e.getX();
-               int y0 = e.getY();
+        int x0 = e.getX();
+        int y0 = e.getY();
 
-               for (int i = 0; i < polygon.npoints; ++i) {
+        for (int i = 0; i < polygon.npoints; ++i) {
+          int x1 = polygon.xpoints[i];
+          int y1 = polygon.ypoints[i];
+          int x2, y2;
+          if (i == polygon.npoints-1) {
+            x2 = polygon.xpoints[0];
+            y2 = polygon.ypoints[0];
+          } 
+          else {
+            x2 = polygon.xpoints[i+1];
+            y2 = polygon.ypoints[i+1];
+          }
 
-                       int x1 = polygon.xpoints[i];
-                       int y1 = polygon.ypoints[i];
-                       int x2, y2;
-                       if (i == polygon.npoints-1) {
-                               x2 = polygon.xpoints[0];
-                               y2 = polygon.ypoints[0];
-                       } 
-                       else {
-                               x2 = polygon.xpoints[i+1];
-                               y2 = polygon.ypoints[i+1];
-                       }
+          if (y2 == y1 && x2 == x1) // two verteces on top of each other: skip
+            continue;
 
-                       if (y2 == y1 && x2 == x1) // two verteces on top of each other: skip
-                               continue;
+          double d = Point2D.distance(x1, y1, x2, y2); // segment length
+          double comp = ((x2-x1)*(x0-x1) + (y2-y1)*(y0-y1)) / d; // component of projection of selection on segment
+          double dist; // orthogonal distance to segment
 
-                       double d = Point2D.distance(x1, y1, x2, y2); // segment length
-                       double comp = ((x2-x1)*(x0-x1) + (y2-y1)*(y0-y1)) / d; // component of projection of selection on segment
-                       double dist; // orthogonal distance to segment
+          if (comp <= 0.0) { // too far out beyond first vertex: just move that vertex if it's closest
+            dist = Point2D.distance(x1, y1, x0, y0);
+            if (dist < minDist) {
+              isVertex = true;
+              minDist = dist;
+              selected = i;
+            }
+          }
+          else if (comp >= d) { // too far out beyond second vertex: just move that vertex: just move that virtex if it's closest
+            dist = Point2D.distance(x0, y0, x2, y2);
+            if (dist < minDist) {
+              isVertex = true;
+              minDist = dist;
+              selected = i+1;
+            }
+          }
+          else { // calculate orthogonal distance to segment
+            dist = Math.abs((y2-y1)*e.getX() - (x2-x1)*e.getY() + x2*y1 - y2*x1) / Math.sqrt((y2-y1)*(y2-y1) + (x2-x1)*(x2-x1));
+            if (dist < minDist) {
+              isVertex = false;
+              minDist = dist;
+              selected = i+1;
+             }
+          }
+        }
 
-                       if (comp <= 0.0) { // too far out beyond first vertex: just move that vertex if it's closest
-                               dist = Point2D.distance(x1, y1, x0, y0);
-                               if (dist < minDist) {
-                                       isVertex = true;
-                                       minDist = dist;
-                                       selected = i;
-                               }
-                       } 
-                       else if (comp >= d) { // too far out beyond second vertex: just move that vertex: just move that virtex if it's closest
-                               dist = Point2D.distance(x0, y0, x2, y2);
-                               if (dist < minDist) {
-                                       isVertex = true;
-                                       minDist = dist;
-                                       selected = i+1;
-                               }
-                       } 
-                       else { // calculate orthogonal distance to segment
-                               dist = Math.abs((y2-y1)*e.getX() - (x2-x1)*e.getY() + x2*y1 - y2*x1) / 
-                                               Math.sqrt((y2-y1)*(y2-y1) + (x2-x1)*(x2-x1));
-                               if (dist < minDist) {
-                                       isVertex = false;
-                                       minDist = dist;
-                                       selected = i+1;
-                               }
-                       }
-               }
-
-               if (!isVertex) { // insert a point near segment
-                       polygon.addPoint(e.getX(), e.getY());
-                       if (selected >= 0) {
-                               for (int i = polygon.npoints - 1; i > selected; --i) {
-                                       polygon.xpoints[i] = polygon.xpoints[i - 1];
-                                       polygon.ypoints[i] = polygon.ypoints[i - 1];
-                               }
-                               polygon.xpoints[selected] = e.getX();
-                               polygon.ypoints[selected] = e.getY();
-                       }
-               }
-       }
+        if (!isVertex) { // insert a point near segment
+          polygon.addPoint(e.getX(), e.getY());
+          if (selected >= 0) {
+            for (int i = polygon.npoints - 1; i > selected; --i) {
+              polygon.xpoints[i] = polygon.xpoints[i - 1];
+              polygon.ypoints[i] = polygon.ypoints[i - 1];
+            }
+            polygon.xpoints[selected] = e.getX();
+            polygon.ypoints[selected] = e.getY();
+          }
+        }
+      }
     }
 
     public void scrollAtEdge(Point evtPt, int dist) {
