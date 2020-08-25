f1ebc00cd8fc15792e085a45b4f422c87c2e5fd8
Brian Reynolds <brian@brianreynolds.net>
2020-08-24 22:22:52 -0400

    Javadoc

--- vassal-app/src/main/java/VASSAL/build/module/map/Flare.java
+++ vassal-app/src/main/java/VASSAL/build/module/map/Flare.java
@@ -57,29 +57,42 @@ import VASSAL.configure.FlareFormattedStringConfigurer;
 import VASSAL.i18n.Resources;
 import VASSAL.tools.swing.SwingUtils;
 
+/**
+ * Allows a player to ping a location ("send up a flare") by clicking on a map with the correct modifier key
+ * combination held down (default: Alt+LeftClick). Can be shown as an animated colored circle, or a plain one.
+ * If reportFormat field is provided, a message is also displayed in the chat log.
+ *
+ * Flare will work with both online play and PBEM play.
+ */
 public class Flare extends AbstractConfigurable
         implements CommandEncoder, GameComponent, Drawable, MouseListener {
   public static final String COMMAND_PREFIX = "FLARE:";
-  private Map map;
-  private int circleSize;
-  private boolean circleScale;
-  private int pulses;
-  private int pulsesPerSec;
-  private boolean animate;
-  private String flareKey;
-  private Color color;
-  private Point clickPoint;
-  private FormattedString reportFormat = new FormattedString();
-  private volatile boolean active;
 
-  public static final String CIRCLE_SIZE  = "circleSize";
-  public static final String CIRCLE_SCALE = "circleScale";
-  public static final String CIRCLE_COLOR = "circleColor";
-  public static final String FLARE_KEY    = "flareKey";
-  public static final String PULSES       = "flarePulses";
+  // Attributes
+  private int circleSize;       // Maximum circle size in pixels
+  private boolean circleScale;  // If true, scale circle to map zoom
+  private int pulses;           // How many total "pulses" to animate, or 0 for steady w/o animation
+  private int pulsesPerSec;     // How many pulses per second, or 0 for steady w/o animation
+  private String flareKey;      // Configures which set of modifier keys and click will produce the flare
+  private Color color;          // Color for the flare circle
+  private FormattedString reportFormat = new FormattedString(); // Report format for reporting the flare to the chat log
+
+  // Internal properties
+  private Map map;                  // The map for this Flare
+  private Point clickPoint;         // Clicked point where this Flare is to appear
+  private boolean animate;          // Internal flag if we should be animating
+  private volatile boolean active;  // Internal flag if we're currently active
+
+  // Attribute names
+  public static final String CIRCLE_SIZE    = "circleSize";
+  public static final String CIRCLE_SCALE   = "circleScale";
+  public static final String CIRCLE_COLOR   = "circleColor";
+  public static final String FLARE_KEY      = "flareKey";
+  public static final String PULSES         = "flarePulses";
   public static final String PULSES_PER_SEC = "flarePulsesPerSec";
-  public static final String REPORT_FORMAT = "reportFormat";
+  public static final String REPORT_FORMAT  = "reportFormat";
 
+  // Friendly (localizable) names for modifier key combinations
   public static final String FLARE_ALT_LOCAL       = Resources.getString("Editor.Flare.flare_key_desc", Resources.getString("Keys.alt"));
   public static final String FLARE_CTRL_LOCAL      = Resources.getString("Editor.Flare.flare_key_desc", Resources.getString("Keys.ctrl"));
   public static final String FLARE_COMMAND_LOCAL   = Resources.getString("Editor.Flare.flare_key_desc", Resources.getString("Keys.meta"));
@@ -91,6 +104,7 @@ public class Flare extends AbstractConfigurable
   public static final String FLARE_ALT_SHIFT_COMMAND_LOCAL = Resources.getString("Editor.Flare.flare_key_desc", Resources.getString("Keys.alt_shift_command"));
   public static final String FLARE_CTRL_ALT_SHIFT_LOCAL = Resources.getString("Editor.Flare.flare_key_desc", Resources.getString("Keys.ctrl_alt_shift"));
 
+  // The modifier key codes we actually store
   public static final String FLARE_ALT            = "keyAlt";
   public static final String FLARE_CTRL           = "keyCtrl";
   public static final String FLARE_ALT_SHIFT      = "keyAltShift";
@@ -98,6 +112,7 @@ public class Flare extends AbstractConfigurable
   public static final String FLARE_CTRL_ALT       = "keyCtrlAlt";
   public static final String FLARE_CTRL_ALT_SHIFT = "keyCtrlAltShift";
 
+  // Special properties for our FormattedString reportFormat
   public static final String FLARE_LOCATION       = "FlareLocation";
   public static final String FLARE_ZONE           = "FlareZone";
   public static final String FLARE_MAP            = "FlareMap";
@@ -114,22 +129,40 @@ public class Flare extends AbstractConfigurable
     pulsesPerSec = 3;
   }
 
+  /**
+   * @return String description of this component, displayed in Editor.
+   */
   public String getDescription() {
     return Resources.getString("Editor.Flare.desc");
   }
 
+  /**
+   * @return String name of component class. The part in [..] in the Editor.
+   */
   public static String getConfigureTypeName() {
     return Resources.getString("Editor.Flare.configure");
   }
 
+  /**
+   * Attribute types for this component's buildFile (xml) entry. These launch the proper configurers when the component is edited in the Editor.
+   * @return list of classes for attributes
+   */
   public Class<?>[] getAttributeTypes() {
     return new Class[] { FlareKeyConfig.class, Integer.class, Color.class, Boolean.class, Integer.class, Integer.class, ReportFormatConfig.class };
   }
 
+  /**
+   * Attribute names for this component's buildFile (xml) entry.
+   * @return list of names for attributes
+   */
   public String[] getAttributeNames() {
     return new String[] { FLARE_KEY, CIRCLE_SIZE, CIRCLE_COLOR, CIRCLE_SCALE, PULSES, PULSES_PER_SEC, REPORT_FORMAT };
   }
 
+  /**
+   * Attribute names for this component's buildFile (xml) entry. These show up in the Editor next to the configurers for each attribute.
+   * @return list of names for attributes
+   */
   public String[] getAttributeDescriptions() {
     return new String[] { Resources.getString("Editor.Flare.flare_key"),
             Resources.getString("Editor.Flare.circle_size"),
@@ -140,6 +173,13 @@ public class Flare extends AbstractConfigurable
             Resources.getString("Editor.report_format")};
   }
 
+  /**
+   * Gets current attribute value in string form.
+   *
+   * @param key - attribute name
+   *
+   * @return current the value of one of this component's attributes, in string form.
+   */
   public String getAttributeValueString(final String key) {
     if (FLARE_KEY.equals(key)) {
       return flareKey;
@@ -165,7 +205,13 @@ public class Flare extends AbstractConfigurable
     return null;
   }
 
-
+  /**
+   * Sets the value of one of this component's attributes.
+   *
+   * @param key the name of the attribute
+   *
+   * @param value new value for attribute. Can pass either the Object itself or the String version.
+   */
   public void setAttribute(String key, Object value) {
     if (FLARE_KEY.equals(key)) {
       if (FLARE_ALT_LOCAL.equals(value)) {
@@ -237,12 +283,18 @@ public class Flare extends AbstractConfigurable
   }
 
 
+  /**
+   * @return Help file for this component, accessed when "Help" button in Editor is clicked while configuring component.
+   */
   @Override
   public HelpFile getHelpFile() {
     return HelpFile.getReferenceManualPage("Flare.htm");  //$NON-NLS-1$
   }
 
-
+  /**
+   * Adds this component to a Buildable component. In this case, a Map.
+   * @param parent - the Map to add the Flare to.
+   */
   public void addTo(final Buildable parent) {
     if (parent instanceof Map) {
       map = (Map) parent;
@@ -255,6 +307,10 @@ public class Flare extends AbstractConfigurable
     }
   }
 
+  /**
+   * Removes this component from a Buildable parent.
+   * @param parent - the Map to remove the Flare from.
+   */
   public void removeFrom(final Buildable parent) {
     if (parent instanceof Map) {
       GameModule.getGameModule().removeCommandEncoder(this);
@@ -274,6 +330,9 @@ public class Flare extends AbstractConfigurable
     ));
   }
 
+  /**
+   * Animator to loop the Flare animation. Use the LOOP behavior so that it's always shrinking bullseye rings.
+   */
   private final Animator animator = new Animator(0, 1, Animator.RepeatBehavior.LOOP, new TimingTargetAdapter() {
     @Override
     public void begin() {
@@ -295,6 +354,11 @@ public class Flare extends AbstractConfigurable
     }
   });
 
+  /**
+   * Start the Flare animation.
+   * @param isLocal - true if this Flare was launched on this client (i.e. by this player). Otherwise, center on
+   *                  the ping location if user preferences are set for centering on opponent moves.
+   */
   public void startAnimation(final boolean isLocal) {
     if (!isLocal) {
       if (GlobalOptions.getInstance().centerOnOpponentsMove()) {
@@ -309,6 +373,11 @@ public class Flare extends AbstractConfigurable
     animator.start();
   }
 
+  /**
+   * Draw the Flare at current animation frame
+   * @param g - Graphics
+   * @param map - Map component
+   */
   public void draw(final Graphics g, final Map map) {
     if (active && clickPoint != null) {
       final Graphics2D g2d = (Graphics2D) g;
@@ -346,6 +415,11 @@ public class Flare extends AbstractConfigurable
     return true;
   }
 
+  /**
+   * Encodes Flare commands into string form.
+   * @param c Command
+   * @return String version of Flare Command, or null if not a Flare Command.
+   */
   public String encode(final Command c) {
     if (c instanceof FlareCommand) {
       return COMMAND_PREFIX + ((FlareCommand) c).getClickPoint().x + "," + ((FlareCommand) c).getClickPoint().y;
@@ -353,6 +427,11 @@ public class Flare extends AbstractConfigurable
     return null;
   }
 
+  /**
+   * Decodes string command info into a Flare Command.
+   * @param s String for a Flare Command
+   * @return Flare Command object
+   */
   public Command decode(final String s) {
     if (s.startsWith(COMMAND_PREFIX)) {
       final int x = Integer.parseInt(s.substring(s.indexOf(":") + 1, s.indexOf(",")));
@@ -370,6 +449,11 @@ public class Flare extends AbstractConfigurable
   public void mouseClicked(final MouseEvent e) {
   }
 
+  /**
+   * Check for our modifier key, and if so launch a Flare sequence on our map, and send a command to other players' along
+   * with a Chat Log message if a Report Format has been specified in the component.
+   * @param e - a MouseEvent
+   */
   public void mousePressed(final MouseEvent e) {
     if (!SwingUtils.isMainMouseButtonDown(e)) {
       return;
@@ -409,18 +493,23 @@ public class Flare extends AbstractConfigurable
     final GameModule mod = GameModule.getGameModule();
 
     Command c = new NullCommand();
+
+    // Set special properties for our reportFormat to use ($FlareLocation$, $FlareZone$, $FlareMap$)
     reportFormat.setProperty(FLARE_LOCATION, map.locationName(clickPoint));
     final Zone z = map.findZone(clickPoint);
     reportFormat.setProperty(FLARE_ZONE, (z != null) ? z.getName() : "");
     reportFormat.setProperty(FLARE_MAP, map.getMapName());
-    String reportText = reportFormat.getLocalizedText(map);
+    String reportText = reportFormat.getLocalizedText(map); // Map and global properties also available (e.g. PlayerName, PlayerSide)
     if (reportText.trim().length() > 0) {
       c = new Chatter.DisplayText(mod.getChatter(), "* " + reportText);
       c.execute();
     }
 
+    // Send to other players
     c = c.append(new FlareCommand(this));
     mod.sendAndLog(c);
+
+    // Start animation on our own client
     startAnimation(true);
   }
 
@@ -448,7 +537,9 @@ public class Flare extends AbstractConfigurable
     return clickPoint;
   }
 
-
+  /**
+   * A configurer for different combinations of modifier key
+   */
   public static class FlareKeyConfig extends StringEnum {
     @Override
     public String[] getValidValues(AutoConfigurable target) {
@@ -463,6 +554,10 @@ public class Flare extends AbstractConfigurable
     }
   }
 
+  /**
+   * A configurer for our reportFormat, which includes the unique $FlareLocation$, $FlareZone$, $FlareMap$ properties as
+   * well as $PlayerName$ and $PlayerSide$ in the "Insert" pulldown.
+   */
   public static class ReportFormatConfig implements TranslatableConfigurerFactory {
     @Override
     public Configurer getConfigurer(AutoConfigurable c, String key, String name) {
