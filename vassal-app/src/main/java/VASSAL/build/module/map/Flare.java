697d4d8805eff308c8905c81643666a8eae8bd50
Brian Reynolds <brian@brianreynolds.net>
2020-08-24 22:52:17 -0400

    Credit the VASL inspiration

--- vassal-app/src/main/java/VASSAL/build/module/map/Flare.java
+++ vassal-app/src/main/java/VASSAL/build/module/map/Flare.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020 Vassalengine.org   Brian Reynolds
+ * Inspired by VASL piece finder by David Sullivan
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
