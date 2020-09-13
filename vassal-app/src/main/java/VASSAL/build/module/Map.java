fdc6f3de76240a4c2777f757db6c0ed4e5d61972
ClaudioC <claudio.ciardelli@gmail.com>
2020-09-13 23:44:15 +0200

    bug13409 fix board.SnapTo error by calling it again

--- vassal-app/src/main/java/VASSAL/build/module/Map.java
+++ vassal-app/src/main/java/VASSAL/build/module/Map.java
@@ -911,6 +911,13 @@ public class Map extends AbstractConfigurable implements GameComponent, MouseLis
     snap.translate(-r.x, -r.y);
     snap = b.snapTo(snap);
     snap.translate(r.x, r.y);
+
+    //CC bugfix13409
+    // If we snapped to a point outside the board b, call sanpTo again with the board we landed into
+    final Board bSnappedTo = findBoard(snap);
+    if (!b.equals(bSnappedTo)) {
+      snap = this.snapTo(snap);
+    } 
     // RFE 882378
     // If we have snapped to a point 1 pixel off the edge of the map, move
     // back
