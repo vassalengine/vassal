b9833d1287b3988d08f06902b3d9bd38c1f48b97
Brian Reynolds <brian@brianreynolds.net>
2020-10-01 17:40:15 -0400

    whoops

--- vassal-app/src/main/java/VASSAL/build/AbstractToolbarItem.java
+++ vassal-app/src/main/java/VASSAL/build/AbstractToolbarItem.java
@@ -30,7 +30,7 @@ import java.awt.Component;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;p
+import java.util.List;
 
 /**
  * Creates an item that is both configurable w/ an edit box {@link AbstractConfigurable} and buildable from the
