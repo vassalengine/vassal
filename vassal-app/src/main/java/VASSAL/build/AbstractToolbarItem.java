143e3b31fc99136ef2895557d7554cf3df64c099
Brian Reynolds <brian@brianreynolds.net>
2020-10-01 17:01:33 -0400

    haha, not rodney, not 2003, but otherwise yeah

--- vassal-app/src/main/java/VASSAL/build/AbstractToolbarItem.java
+++ vassal-app/src/main/java/VASSAL/build/AbstractToolbarItem.java
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2000-2003 by Rodney Kinney
+ * Copyright (c) 2020 by vassalengine.org, Brian Reynolds
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
