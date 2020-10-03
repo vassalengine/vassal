ad099372ded0dc8b39ac352ebdad136c762db8bc
Brian Reynolds <brian@brianreynolds.net>
2020-10-03 12:30:13 -0400

    The Dreaded UI - Part 1: Cheap and Cheerful

--- vassal-app/src/main/java/VASSAL/configure/RemoveUnusedImagesDialog.java
+++ vassal-app/src/main/java/VASSAL/configure/RemoveUnusedImagesDialog.java
@@ -37,22 +37,92 @@ public class RemoveUnusedImagesDialog extends JDialog {
     super(owner, "Remove Unused Images", true);
   
     final JLabel text =
-      new JLabel("The following images are unused. Remove them?");
+      new JLabel("The following image files appear to be unreferenced from the module. They may however be" +
+        "referenced in custom code, or be the masks for Non-Rectangular traits.");
+
+    final JLabel keepy   = new JLabel("Files to keep");
+    final JLabel excrete = new JLabel("Files to remove");
 
     final GameModule module = GameModule.getGameModule();
-    final SortedSet<String> unused = new TreeSet<>();
+    SortedSet<String> unused = new TreeSet<>();
+    SortedSet<String> buggerOff = new TreeSet<>();
     Collections.addAll(unused, module.getDataArchive().getImageNames());
     unused.removeAll(module.getAllImageNames());
 
-    final DefaultListModel lm = new DefaultListModel();
+    DefaultListModel lm = new DefaultListModel();
     for (String iname : unused) {
       lm.addElement(iname);
     }
-    
-    final JList list = new JList(lm);
-    final JScrollPane scroll = new JScrollPane(list);
- 
-    final JButton ok = new JButton("Ok");
+
+    DefaultListModel lm2 = new DefaultListModel();
+
+    final JList keepyMcKeepface = new JList(lm);
+    final JScrollPane scroll = new JScrollPane(keepyMcKeepface);
+
+    final JList excretable = new JList(lm2);
+    final JScrollPane scroll2 = new JScrollPane(excretable);
+
+    final JButton bugger = new JButton ("-->");
+    bugger.addActionListener(e -> {
+      int[] indices = keepyMcKeepface.getSelectedIndices();
+      int lastSelect = -1;
+      for (int idx : indices) {
+        String thing = (String)lm.getElementAt(idx);
+        unused.remove(thing);
+        buggerOff.add(thing);
+        if (lastSelect < 0) {
+          lastSelect = idx;
+        }
+      }
+      lm.removeAllElements();
+      lm2.removeAllElements();
+      for (String iname : unused) {
+        lm.addElement(iname);
+      }
+      for (String iname : buggerOff) {
+        lm2.addElement(iname);
+      }
+      if (lastSelect >= 0) {
+        if (lastSelect < keepyMcKeepface.getModel().getSize()) {
+          keepyMcKeepface.setSelectedIndex(lastSelect);
+        }
+        else if (lastSelect > 0) {
+          keepyMcKeepface.setSelectedIndex(lastSelect - 1);
+        }
+      }
+    });
+
+    final JButton unBugger = new JButton ("<--");
+    unBugger.addActionListener(e -> {
+      int[] indices = excretable.getSelectedIndices();
+      int lastSelect = -1;
+      for (int idx : indices) {
+        String thing = (String)lm2.getElementAt(idx);
+        buggerOff.remove(thing);
+        unused.add(thing);
+        if (lastSelect < 0) {
+          lastSelect = idx;
+        }
+      }
+      lm.removeAllElements();
+      lm2.removeAllElements();
+      for (String iname : unused) {
+        lm.addElement(iname);
+      }
+      for (String iname : buggerOff) {
+        lm2.addElement(iname);
+      }
+      if (lastSelect >= 0) {
+        if (lastSelect < excretable.getModel().getSize()) {
+          excretable.setSelectedIndex(lastSelect);
+        }
+        else if (lastSelect > 0) {
+          excretable.setSelectedIndex(lastSelect - 1);
+        }
+      }
+    });
+
+    final JButton ok = new JButton("Remove Files");
     ok.addActionListener(e -> {
       final ArchiveWriter aw = module.getDataArchive().getWriter();
 
@@ -60,8 +130,9 @@ public class RemoveUnusedImagesDialog extends JDialog {
         new File(new File(aw.getName()).getParent(), "removed");
       dir.mkdir();
 
-      for (String u : unused) {
-        System.out.println("removing: " + u);
+      for (String u : buggerOff) {
+        GameModule.getGameModule().warn("- Removing: " + u);
+        System.out.println("Removing: " + u);
 
         InputStream in = null;
         FileOutputStream out = null;
@@ -97,7 +168,18 @@ public class RemoveUnusedImagesDialog extends JDialog {
     layout.setHorizontalGroup(
       layout.createParallelGroup(GroupLayout.Alignment.LEADING, true)
         .addComponent(text)
-        .addComponent(scroll)
+        .addComponent(keepy)
+        .addComponent(excrete)
+        .addGroup(layout.createSequentialGroup()
+          .addGap(0, 0, Integer.MAX_VALUE)
+          .addComponent(scroll)
+          .addGap(0, 0, Integer.MAX_VALUE)
+          .addGroup(layout.createSequentialGroup()
+            .addComponent(bugger)
+            .addComponent(unBugger))
+          .addGap(0, 0, Integer.MAX_VALUE)
+          .addComponent(scroll2)
+          .addGap(0, 0, Integer.MAX_VALUE))
         .addGroup(layout.createSequentialGroup()
           .addGap(0, 0, Integer.MAX_VALUE)
           .addComponent(ok)
@@ -107,7 +189,13 @@ public class RemoveUnusedImagesDialog extends JDialog {
     layout.setVerticalGroup(
       layout.createSequentialGroup()
         .addComponent(text)
+        .addComponent(keepy)
+        .addComponent(excrete)
         .addComponent(scroll)
+        .addGroup(layout.createSequentialGroup()
+          .addComponent(bugger)
+          .addComponent(unBugger))
+        .addComponent(scroll2)
         .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
         .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE, false)
           .addComponent(ok)
