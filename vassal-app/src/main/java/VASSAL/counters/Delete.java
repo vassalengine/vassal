b756a908f24d4b414473e4e783aa830c6668d459
Jason Stewart <jason.e.stewart@gmail.com>
2020-10-09 16:42:15 +0100

    fixed javadoc

--- vassal-app/src/main/java/VASSAL/counters/Delete.java
+++ vassal-app/src/main/java/VASSAL/counters/Delete.java
@@ -41,7 +41,7 @@ import VASSAL.tools.NamedKeyStroke;
 import VASSAL.tools.SequenceEncoder;
 
 /**
- * This trait adds a command that creates a duplicate of the selected Gamepiece
+ * This trait adds a command that deletes the selected Gamepiece
  */
 public class Delete extends Decorator implements TranslatablePiece {
   public static final String ID = "delete;"; // NON-NLS
@@ -76,7 +76,7 @@ public class Delete extends Decorator implements TranslatablePiece {
   }
 
   @Override
-  protected KeyCommand[] myGetKeyCommands() {
+  public KeyCommand[] myGetKeyCommands() {
     if (keyCommands == null) {
       deleteCommand = new KeyCommand(commandName, key, Decorator.getOutermost(this), this);
       if (commandName.length() > 0 && key != null && ! key.isNull()) {
