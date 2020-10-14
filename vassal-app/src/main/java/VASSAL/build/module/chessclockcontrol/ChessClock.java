704407edfbffcfc6e22e8475e1f8a1b3d2aea057
Brian Reynolds <brian@brianreynolds.net>
2020-10-14 11:13:40 -0400

    stop failing the build

--- vassal-app/src/main/java/VASSAL/build/module/chessclockcontrol/ChessClock.java
+++ vassal-app/src/main/java/VASSAL/build/module/chessclockcontrol/ChessClock.java
@@ -20,7 +20,6 @@ package VASSAL.build.module.chessclockcontrol;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -454,15 +453,15 @@ public class ChessClock extends AbstractConfigurable implements CommandEncoder, 
     if (doDays) {
       if (doTenths) {
         timerButton.setText(String.format("%s%d:%02d:%02d:%02d.%d", getFormattedButtonText(), days, hours, minutes, seconds, tenths)); //NON-NLS
-        baseline = "0:00:00:00.0";
+        baseline = "0:00:00:00.0"; //NON-NLS
       }
       else if (doSeconds) {
         timerButton.setText(String.format("%s%d:%02d:%02d:%02d", getFormattedButtonText(), days, hours, minutes, seconds)); //NON-NLS
-        baseline = "0:00:00:00.0";
+        baseline = "0:00:00:00.0"; //NON-NLS
       }
       else {
         timerButton.setText(String.format("%s%dd:%02d:%02d", getFormattedButtonText(), days, hours, minutes)); //NON-NLS
-        baseline = "0d:00:00";
+        baseline = "0d:00:00"; //NON-NLS
       }
     }
     else {
@@ -471,28 +470,28 @@ public class ChessClock extends AbstractConfigurable implements CommandEncoder, 
         if (doTenths) {
           timerButton.setText(String.format("%s%d:%02d:%02d.%d", getFormattedButtonText(), hours, minutes, seconds, tenths)); //NON-NLS
           if (hours >= 10) {
-            baseline = String.format("%d0:00:00.0", hours/10);
+            baseline = String.format("%d0:00:00.0", hours/10); //NON-NLS
           }
           else {
-            baseline = "0:00:00.0";
+            baseline = "0:00:00.0"; //NON-NLS
           }
         }
         else if (doSeconds) {
           timerButton.setText(String.format("%s%d:%02d:%02d", getFormattedButtonText(), hours, minutes, seconds)); //NON-NLS
           if (hours >= 10) {
-            baseline = String.format("%d0:00:00", hours/10);
+            baseline = String.format("%d0:00:00", hours/10); //NON-NLS
           }
           else {
-            baseline = "0:00:00";
+            baseline = "0:00:00"; //NON-NLS
           }
         }
         else {
           timerButton.setText(String.format("%s%d:%02d", getFormattedButtonText(), hours, minutes)); //NON-NLS
           if (hours >= 10) {
-            baseline = String.format("%d0:00", hours/10);
+            baseline = String.format("%d0:00", hours/10); //NON-NLS
           }
           else {
-            baseline = "0:00";
+            baseline = "0:00"; //NON-NLS
           }
         }
       }
@@ -502,26 +501,23 @@ public class ChessClock extends AbstractConfigurable implements CommandEncoder, 
         if (doTenths) {
           timerButton.setText(String.format("%s%d:%02d.%d", getFormattedButtonText(), minutes, seconds, tenths)); //NON-NLS
           if (minutes >= 10) {
-            baseline = String.format("%d0:00.0", minutes/10);
+            baseline = String.format("%d0:00.0", minutes/10); //NON-NLS
           }
           else {
-            baseline = String.format("0:00.0");
+            baseline = String.format("0:00.0"); //NON-NLS
           }
         }
         else {
           timerButton.setText(String.format("%s%d:%02d", getFormattedButtonText(), minutes, seconds)); //NON-NLS
           if (minutes >= 10) {
-            baseline = String.format("%d0:00", minutes/10);
+            baseline = String.format("%d0:00", minutes/10); //NON-NLS
           }
           else {
-            baseline = String.format("0:00");
+            baseline = String.format("0:00"); //NON-NLS
           }
         }
       }
     }
-
-    //Graphics g = timerButton.getGraphics();
-    //int width = g.getFontMetrics().stringWidth(getFormattedButtonText() + baseline);
 
     int width = timerButton.getFontMetrics(timerButton.getFont()).stringWidth(getFormattedButtonText() + baseline);
     timerButton.setMinimumSize(new Dimension(width, timerButton.getMinimumSize().height));
