b73731ae9e1789a694447a9a3cff61da80a6cca2
claudiociardelli <claudio.ciardelli@gmail.com>
2021-02-08 09:41:58 +0100

    Stricter filename check

--- vassal-app/src/main/java/VASSAL/configure/RefreshPredefinedSetupsDialog.java
+++ vassal-app/src/main/java/VASSAL/configure/RefreshPredefinedSetupsDialog.java
@@ -135,7 +135,7 @@ public class RefreshPredefinedSetupsDialog extends JDialog {
         //Exclude scenario folders (isMenu == true)
         // and exclude any "New game" entries (no predefined setup) (isUseFile == false)
         // !! Some New Game entries have UseFile = true and filename empty. Check file name too
-        if (pds.getFileName() != null) {
+        if (pds.getFileName() != null && ! pds.getFileName().isBlank()) {
           modulePds.add(pds);
         }
       }
