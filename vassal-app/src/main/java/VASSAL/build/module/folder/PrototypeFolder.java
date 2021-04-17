6fc935030f963bc9ff4e25bf84cc79186c21d230
Brian Reynolds <brian@brianreynolds.net>
2021-04-16 23:25:32 -0400

    Prototypes

---
+++ vassal-app/src/main/java/VASSAL/build/module/folder/PrototypeFolder.java
@@ -0,0 +1,45 @@
+/*
+ *
+ * Copyright (c) 2021 by vassalengine.org, Brian Reynolds
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+
+package VASSAL.build.module.folder;
+
+import VASSAL.build.AbstractFolder;
+import VASSAL.build.Buildable;
+import VASSAL.build.module.PrototypeDefinition;
+import VASSAL.build.module.PrototypesContainer;
+import VASSAL.i18n.Resources;
+
+public class PrototypeFolder extends AbstractFolder {
+  @Override
+  public Class<?>[] getAllowableConfigureComponents() {
+    return new Class<?>[] { PrototypeDefinition.class, this.getClass()};
+  }
+
+  public static String getConfigureTypeName() {
+    return Resources.getString("Editor.Folder.component_type", PrototypeDefinition.getConfigureTypeName()); //$NON-NLS-1$
+  }
+
+  @Override
+  public void add(Buildable b) {
+    super.add(b);
+    if (b instanceof PrototypeDefinition) {
+      PrototypesContainer protos = (PrototypesContainer)getNonFolderAncestor();
+      protos.addDefinition((PrototypeDefinition) b);
+    }
+  }
+}
