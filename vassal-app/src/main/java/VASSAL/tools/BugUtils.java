50e502534d99714af9033dac8bd867a8fb053bb0
Joel Uckelman <uckelman@nomic.net>
2021-06-28 00:06:46 +0100

    BugUtils is intended to be entirely static; prevent anyone from making an instance.

--- vassal-app/src/main/java/VASSAL/tools/BugUtils.java
+++ vassal-app/src/main/java/VASSAL/tools/BugUtils.java
@@ -14,6 +14,7 @@ import VASSAL.Info;
 import VASSAL.build.GameModule;
 
 public class BugUtils {
+  private BugUtils() {}
 
   public static void sendBugReport(String email,
                                    String description,
