285996cfe06a063e70822fb0351a3e59638b5d0f
Joel Uckelman <uckelman@nomic.net>
2021-07-29 13:48:53 +0100

    Added java.home to startup errorLog output so we have a chance of telling what JVM is in use.

--- vassal-app/src/main/java/VASSAL/launch/StartUp.java
+++ vassal-app/src/main/java/VASSAL/launch/StartUp.java
@@ -175,6 +175,7 @@ public class StartUp {
                       + System.getProperty("os.version") + " " //$NON-NLS-1$ //$NON-NLS-2$
                       + System.getProperty("os.arch")); //$NON-NLS-1$
     logger.info("Java version " + System.getProperty("java.version")); //$NON-NLS-1$ //$NON-NLS-2$
+    logger.info("Java home " + System.getProperty("java.home")); //$NON-NLS-1$ //$NON-NLS-2$
     logger.info("VASSAL version " + Info.getVersion()); //$NON-NLS-1$
   }
 }
