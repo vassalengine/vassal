7842336a13cf0fde2dc147a481f68c21f06e3371
Brian Reynolds <brian@brianreynolds.net>
2021-08-01 23:18:30 -0400

    unused imports

--- vassal-app/src/main/java/VASSAL/build/module/map/boardPicker/board/RegionGrid.java
+++ vassal-app/src/main/java/VASSAL/build/module/map/boardPicker/board/RegionGrid.java
@@ -27,7 +27,6 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.Stroke;
 import java.awt.datatransfer.StringSelection;
