a5951c6a838b9beaf18435c14e58340bf213a58a
Brent Easton <b.easton@exemail.com.au>
2021-10-11 21:27:10 +1100

    10519 Improve movement of Mats

--- vassal-app/src/main/java/VASSAL/counters/MatCargo.java
+++ vassal-app/src/main/java/VASSAL/counters/MatCargo.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2021 by The VASSAL Development team
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
 package VASSAL.counters;
 
 import java.awt.Component;
