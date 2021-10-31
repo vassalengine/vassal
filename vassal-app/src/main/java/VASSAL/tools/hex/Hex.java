ecb2cb31f8cefb438e4d628f67ae8ebecf8fb596
Brent Easton <b.easton@exemail.com.au>
2021-11-01 08:23:49 +1100

    9589 Calculate Hex Range correctly, regardless of hex shape.

---
+++ vassal-app/src/main/java/VASSAL/tools/hex/Hex.java
@@ -0,0 +1,63 @@
+// Generated code -- CC0 -- No Rights Reserved -- http://www.redblobgames.com/grids/hexagons/
+// Hex co-ordinates - 3D Scheme
+
+package VASSAL.tools.hex;
+
+import java.util.ArrayList;
+
+public class Hex {
+  public final int q;
+  public final int r;
+  public final int s;
+
+  public Hex(int q, int r, int s) {
+    this.q = q;
+    this.r = r;
+    this.s = s;
+    if (q + r + s != 0) throw new IllegalArgumentException("q + r + s must be 0");
+  }
+
+  public Hex add(Hex b) {
+    return new Hex(q + b.q, r + b.r, s + b.s);
+  }
+
+  public Hex subtract(Hex b) {
+    return new Hex(q - b.q, r - b.r, s - b.s);
+  }
+
+  public Hex scale(int k) {
+    return new Hex(q * k, r * k, s * k);
+  }
+
+  public Hex rotateLeft() {
+    return new Hex(-s, -q, -r);
+  }
+
+  public Hex rotateRight() {
+    return new Hex(-r, -s, -q);
+  }
+  static public ArrayList<Hex> directions = new ArrayList<Hex>(){{add(new Hex(1, 0, -1)); add(new Hex(1, -1, 0)); add(new Hex(0, -1, 1)); add(new Hex(-1, 0, 1)); add(new Hex(-1, 1, 0)); add(new Hex(0, 1, -1));}};
+
+  static public Hex direction(int direction) {
+    return Hex.directions.get(direction);
+  }
+
+  public Hex neighbor(int direction) {
+    return add(Hex.direction(direction));
+  }
+
+  static public ArrayList<Hex> diagonals = new ArrayList<Hex>(){{add(new Hex(2, -1, -1)); add(new Hex(1, -2, 1)); add(new Hex(-1, -1, 2)); add(new Hex(-2, 1, 1)); add(new Hex(-1, 2, -1)); add(new Hex(1, 1, -2));}};
+
+  public Hex diagonalNeighbor(int direction) {
+    return add(Hex.diagonals.get(direction));
+  }
+
+  public int length() {
+    return (int)((Math.abs(q) + Math.abs(r) + Math.abs(s)) / 2);
+  }
+
+  public int distance(Hex b) {
+    return subtract(b).length();
+  }
+
+}
---
+++ vassal-app/src/main/java/VASSAL/tools/hex/OffsetCoord.java
@@ -0,0 +1,61 @@
+// Generated code -- CC0 -- No Rights Reserved -- http://www.redblobgames.com/grids/hexagons/
+// Hex co-ordinates - Offset Scheme
+
+package VASSAL.tools.hex;
+
+public class OffsetCoord {
+
+  public OffsetCoord(int col, int row)
+  {
+    this.col = col;
+    this.row = row;
+  }
+
+  public final int col;
+  public final int row;
+  static public int EVEN = 1;
+  static public int ODD = -1;
+
+  static public OffsetCoord qoffsetFromCube(int offset, Hex h) {
+    int col = h.q;
+    int row = h.r + (int)((h.q + offset * (h.q & 1)) / 2);
+    if (offset != OffsetCoord.EVEN && offset != OffsetCoord.ODD)
+    {
+      throw new IllegalArgumentException("offset must be EVEN (+1) or ODD (-1)");
+    }
+    return new OffsetCoord(col, row);
+  }
+
+  static public Hex qoffsetToCube(int offset, OffsetCoord h) {
+    int q = h.col;
+    int r = h.row - (int)((h.col + offset * (h.col & 1)) / 2);
+    int s = -q - r;
+    if (offset != OffsetCoord.EVEN && offset != OffsetCoord.ODD)
+    {
+      throw new IllegalArgumentException("offset must be EVEN (+1) or ODD (-1)");
+    }
+    return new Hex(q, r, s);
+  }
+
+  static public OffsetCoord roffsetFromCube(int offset, Hex h) {
+    int col = h.q + (int)((h.r + offset * (h.r & 1)) / 2);
+    int row = h.r;
+    if (offset != OffsetCoord.EVEN && offset != OffsetCoord.ODD)
+    {
+      throw new IllegalArgumentException("offset must be EVEN (+1) or ODD (-1)");
+    }
+    return new OffsetCoord(col, row);
+  }
+
+  static public Hex roffsetToCube(int offset, OffsetCoord h) {
+    int q = h.col - (int)((h.row + offset * (h.row & 1)) / 2);
+    int r = h.row;
+    int s = -q - r;
+    if (offset != OffsetCoord.EVEN && offset != OffsetCoord.ODD)
+    {
+      throw new IllegalArgumentException("offset must be EVEN (+1) or ODD (-1)");
+    }
+    return new Hex(q, r, s);
+  }
+
+}
