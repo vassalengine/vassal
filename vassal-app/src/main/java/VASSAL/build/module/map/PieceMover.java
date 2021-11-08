6c3506027eed4c5d97faaee9aa1ec0603821eeff
Brian Reynolds <brian@brianreynolds.net>
2021-11-08 10:38:05 -0500

    Better behavior when dragged to an inaccessible deck

--- vassal-app/src/main/java/VASSAL/build/module/map/PieceMover.java
+++ vassal-app/src/main/java/VASSAL/build/module/map/PieceMover.java
@@ -312,10 +312,6 @@ public class PieceMover extends AbstractBuildable
      */
     @Override
     public Object visitDeck(Deck d) {
-      // Don't allow drag to deck if we don't have access to it
-      if (!d.isAccessible()) {
-        return null;
-      }
       final Point pos = d.getPosition();
       final Point p = new Point(pt.x - pos.x, pt.y - pos.y);
       return d.getShape().contains(p) ? d : null;
@@ -1086,7 +1082,7 @@ public class PieceMover extends AbstractBuildable
         if (mergeWith instanceof Deck) {
           final ArrayList<GamePiece> newList = new ArrayList<>(0);
           for (final GamePiece piece : draggedPieces) {
-            if (((Deck) mergeWith).mayContain(piece)) {
+            if (((Deck) mergeWith).mayContain(piece) && ((Deck) mergeWith).isAccessible()) {
               final boolean isObscuredToMe = Boolean.TRUE.equals(piece.getProperty(Properties.OBSCURED_TO_ME));
               if (!isObscuredToMe || Deck.NO_USER.equals(piece.getProperty(Properties.OBSCURED_BY))) {
                 newList.add(piece);
