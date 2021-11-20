f5c2f6be171753efde2f63c83e1c137f7242b1b3
Brian Reynolds <brian@brianreynolds.net>
2021-11-20 13:03:38 -0500

    Action Buttons now respect rotation. Also fixed a bug in getOuter()

--- vassal-app/src/main/java/VASSAL/counters/ActionButton.java
+++ vassal-app/src/main/java/VASSAL/counters/ActionButton.java
@@ -41,6 +41,7 @@ import java.awt.Shape;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
