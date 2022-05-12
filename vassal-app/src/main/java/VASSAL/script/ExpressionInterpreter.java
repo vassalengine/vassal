57afe50d99eea2314f7da46af9d0ff92f5b77578
Brent Easton <b.easton@exemail.com.au>
2022-05-13 08:05:49 +1000

    Do not convert a single 0 to a String

--- vassal-app/src/main/java/VASSAL/script/ExpressionInterpreter.java
+++ vassal-app/src/main/java/VASSAL/script/ExpressionInterpreter.java
@@ -289,7 +289,7 @@ public class ExpressionInterpreter extends AbstractInterpreter implements Loopab
         }
         else {
           try {
-            if (value.length() > 0 && StringUtils.containsOnly(value.substring(0, 1), "+-0")) {
+            if (value.length() > 1 && StringUtils.containsOnly(value.substring(0, 1), "+-0")) {
               setVar(var, value);
             }
             else {
