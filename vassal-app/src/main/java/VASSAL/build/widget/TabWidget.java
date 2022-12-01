5d32fcc186d0907406a9f2739e4b7891b203b600
Joel Uckelman <uckelman@nomic.net>
2022-12-01 21:57:42 +0000

    Fixed NPE: tab.getTitleAt() can return null, so check for that.

--- vassal-app/src/main/java/VASSAL/build/widget/TabWidget.java
+++ vassal-app/src/main/java/VASSAL/build/widget/TabWidget.java
@@ -113,7 +113,11 @@ public class TabWidget extends Widget
       tab.setTitleAt(index, "");
       // In Play mode, the title may have been translated, so need to keep the existing title
       // In Edit mode, the title will not have been set yet, so need to reset it to the actual title
-      tab.setTitleAt(index, currentTitle.isEmpty() ? w.getConfigureName() : currentTitle);
+      tab.setTitleAt(
+        index,
+        currentTitle == null || currentTitle.isEmpty() ?
+          w.getConfigureName() : currentTitle
+      );
       index++;
     }
   }
