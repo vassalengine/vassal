d950010e443f3f64218cafeda629685c939e48e3
Brent <b.easton@esternsydney.edu.au>
2023-01-08 09:31:50 +1100

    11813 - NullPointerException in PieceSlot.getLocalizedConfigureName

--- vassal-app/src/main/java/VASSAL/build/widget/PieceSlot.java
+++ vassal-app/src/main/java/VASSAL/build/widget/PieceSlot.java
@@ -606,13 +606,18 @@ public class PieceSlot extends Widget implements MouseListener, KeyListener {
 
   @Override
   public String getLocalizedConfigureName() {
-    final GamePiece p = Decorator.getOutermost(getExpandedPiece());
-    final String translatedBasicName = (String)p.getLocalizedProperty(BasicPiece.BASIC_NAME);
-    if ((translatedBasicName != null) && !translatedBasicName.isBlank()) {
-      return translatedBasicName;
+    if (getPiece() != null) {
+      final GamePiece p = Decorator.getOutermost(getExpandedPiece());
+      final String translatedBasicName = (String) p.getLocalizedProperty(BasicPiece.BASIC_NAME);
+      if ((translatedBasicName != null) && !translatedBasicName.isBlank()) {
+        return translatedBasicName;
+      }
+      final String translatedName = p.getLocalizedName();
+      return (translatedName == null || translatedName.isBlank()) ? super.getLocalizedConfigureName() : translatedName;
     }
-    final String translatedName = p.getLocalizedName();
-    return (translatedName == null || translatedName.isBlank()) ? super.getLocalizedConfigureName() : translatedName;
+    else {
+      return getConfigureName();
+    }
   }
 
   @Override
