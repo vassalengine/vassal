9a8058f7435a5dfa2fea3a4a3eed14499c3ad329
Joel Uckelman <uckelman@nomic.net>
2023-04-17 21:30:22 +0100

    Added VASSAL.noagent system property for disabling the Windows java agent.

--- vassal-app/src/main/java/VASSAL/launch/AbstractLaunchAction.java
+++ vassal-app/src/main/java/VASSAL/launch/AbstractLaunchAction.java
@@ -553,9 +553,12 @@ public abstract class AbstractLaunchAction extends AbstractAction {
       result.add("");   // reserved for maximum heap
 
       if (SystemUtils.IS_OS_WINDOWS) {
-        result.add("-javaagent:lib\\vassal-agent.jar");
-        result.add("--add-opens");
-        result.add("java.desktop/sun.awt.shell=ALL-UNNAMED");
+        final String noAgent = System.getProperty("VASSAL.noagent");
+        if (noAgent != null) {
+          result.add("-javaagent:lib\\vassal-agent.jar");
+          result.add("--add-opens");
+          result.add("java.desktop/sun.awt.shell=ALL-UNNAMED");
+        }
       }
 
       result.addAll(new CustomVmOptions().getCustomVmOptions());
