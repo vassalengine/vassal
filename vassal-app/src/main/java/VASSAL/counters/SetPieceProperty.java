65bb92f87ffcdf90421a9423925eea062e133b45
Brian Reynolds <brian@brianreynolds.net>
2023-04-22 16:35:08 -0400

    a substantial push

--- vassal-app/src/main/java/VASSAL/counters/SetPieceProperty.java
+++ vassal-app/src/main/java/VASSAL/counters/SetPieceProperty.java
@@ -17,9 +17,10 @@
  */
 package VASSAL.counters;
 
+import VASSAL.build.module.Map;
 import VASSAL.build.module.documentation.HelpFile;
+import VASSAL.build.module.properties.PropertyChanger;
 import VASSAL.build.module.properties.PropertyPrompt;
-import VASSAL.build.module.properties.PropertySource;
 import VASSAL.command.ChangeTracker;
 import VASSAL.command.Command;
 import VASSAL.command.NullCommand;
@@ -27,10 +28,13 @@ import VASSAL.configure.BooleanConfigurer;
 import VASSAL.configure.DynamicKeyCommandListConfigurer;
 import VASSAL.configure.FormattedExpressionConfigurer;
 import VASSAL.configure.IntConfigurer;
+import VASSAL.configure.PropertyExpression;
 import VASSAL.configure.StringConfigurer;
 import VASSAL.configure.TranslatingStringEnumConfigurer;
 import VASSAL.i18n.Resources;
+import VASSAL.script.expression.AuditTrail;
 import VASSAL.tools.FormattedString;
+import VASSAL.tools.RecursionLimiter;
 import VASSAL.tools.SequenceEncoder;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -48,9 +52,9 @@ import java.util.Objects;
  * @author Brian Reynolds, Brent Easton
  *
  * A trait that allows counters to manipulate the value of the Dynamic Properties of OTHER pieces.
- * Uses the Property manipulation functionality of DynamicProperty
+ * Combines the Property manipulation functionality of DynamicProperty with the searching function of Global Key Commands
  */
-public class SetPieceProperty extends DynamicProperty {
+public class SetPieceProperty extends DynamicProperty implements RecursionLimiter.Loopable {
   protected PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
   public static final String ID = "setpieceprop;"; // NON-NLS
   public static final String INDEX_PROP = "AttachmentIndex"; //NON-NLS
@@ -58,47 +62,15 @@ public class SetPieceProperty extends DynamicProperty {
   public static final String ATTACH_BASIC = "AttachmentBasicName"; //NON-NLS
   protected String description;
 
-  protected String attachName;
-  protected String attachIndex;
-  
+  protected GlobalCommandTarget target = new GlobalCommandTarget(GlobalCommandTarget.GKCtype.COUNTER);
+  protected GlobalSetter globalSetter = new GlobalSetter(this);
+  protected PropertyExpression propertiesFilter = new PropertyExpression();
+  protected boolean restrictRange;
+  protected boolean fixedRange = true;
+  protected int range;
+  protected String rangeProperty = "";
+
   protected Decorator dec;
-
-  private final SetPiecePropertySource propertiesWithIndex = new SetPiecePropertySource();
-  private int index = 0;
-  private String currentBasicName = "";
-  private String currentAttachmentName = "";
-  private final SetPieceProperty This = this; // So our property source thing can report our identity downstream
-
-  /**
-   * Makes our special location-currently-being-evaluated information available to our property-match evaluation; other than that, properties from the piece as usual
-   */
-  public class SetPiecePropertySource implements PropertySource {
-    @Override
-    public Object getProperty(Object key) {
-      if (INDEX_PROP.equals(key)) {
-        return index;
-      }
-      else if (ATTACH_PROP.equals(key)) {
-        return currentAttachmentName;
-      }
-      else if (ATTACH_BASIC.equals(key)) {
-        return currentBasicName;
-      }
-      else {
-        return Decorator.getOutermost(piece).getProperty(key);
-      }
-    }
-
-    public GamePiece getPiece() {
-      return This;
-    }
-
-    @Override
-    public Object getLocalizedProperty(Object key) {
-      return getProperty(key);
-    }
-  }
-
 
   public SetPieceProperty() {
     this(ID, null);
@@ -137,8 +109,17 @@ public class SetPieceProperty extends DynamicProperty {
     ).toArray(KeyCommand[]::new);
 
     description = sd.nextToken("");
-    attachName = sd.nextToken("");
-    attachIndex = sd.nextToken("");
+
+    target.decode(sd.nextToken(""));
+    target.setGKCtype(GlobalCommandTarget.GKCtype.COUNTER);
+    target.setCurPiece(this);
+
+    propertiesFilter.setExpression(sd.nextToken(""));
+    restrictRange = sd.nextBoolean(false);
+    range = sd.nextInt(1);
+    fixedRange = sd.nextBoolean(true);
+    rangeProperty = sd.nextToken("");
+    globalSetter.setSelectFromDeckExpression(sd.nextToken("-1"));
   }
 
   @Override
@@ -148,8 +129,13 @@ public class SetPieceProperty extends DynamicProperty {
     se.append(encodeConstraints());
     se.append(keyCommandListConfig.getValueString());
     se.append(description);
-    se.append(attachName);
-    se.append(attachIndex);
+    se.append(target.encode());
+    se.append(propertiesFilter);
+    se.append(restrictRange);
+    se.append(range);
+    se.append(fixedRange);
+    se.append(rangeProperty);
+    se.append(globalSetter.getSelectFromDeckExpression());
     return ID + se.getValue();
   }
 
@@ -180,15 +166,6 @@ public class SetPieceProperty extends DynamicProperty {
     else if (Properties.VISIBLE_STATE.equals(key)) {
       return myGetState() + piece.getProperty(key);
     }
-    else if (INDEX_PROP.equals(key)) {
-      return index;
-    }
-    else if (ATTACH_PROP.equals(key)) {
-      return currentAttachmentName;
-    }
-    else if (ATTACH_BASIC.equals(key)) {
-      return currentBasicName;
-    }
     else {
       return piece.getProperty(key);
     }
@@ -207,15 +184,6 @@ public class SetPieceProperty extends DynamicProperty {
     }
     else if (Properties.VISIBLE_STATE.equals(key)) {
       return getProperty(key);
-    }
-    else if (INDEX_PROP.equals(key)) {
-      return index;
-    }
-    else if (ATTACH_PROP.equals(key)) {
-      return currentAttachmentName;
-    }
-    else if (ATTACH_BASIC.equals(key)) {
-      return currentBasicName;
     }
     else {
       return piece.getLocalizedProperty(key);
@@ -240,6 +208,41 @@ public class SetPieceProperty extends DynamicProperty {
     return HelpFile.getReferenceManualPage("SetPieceProperty.html"); // NON-NLS
   }
 
+  private String propName;
+  private PropertyChanger changer = null;
+  private String newValue = null;
+
+  public Command makeSetTargetCommand(GamePiece p) {
+    Command comm = new NullCommand();
+
+    while (p instanceof Decorator) {
+      if (p instanceof DynamicProperty) {
+        final DynamicProperty currentDP = (DynamicProperty)p;
+
+        if (propName.equals(currentDP.getName())) {
+          if ((newValue == null) || !(changer instanceof PropertyPrompt)) {
+            newValue = changer.getNewValue(currentDP.value);
+            // The PropertyChanger has already evaluated any Beanshell, only need to handle any remaining $$variables.
+            if (newValue.indexOf('$') >= 0) {
+              format.setFormat(newValue);
+              newValue = format.getText(this, this, "Editor.PropertyChangeConfigurer.new_value");
+            }
+          }
+
+          final ChangeTracker ct = new ChangeTracker(currentDP);
+
+          currentDP.value = newValue;
+
+          comm = comm.append(ct.getChangeCommand());
+        }
+      }
+
+      p = ((Decorator) p).getInner();
+    }
+
+    return comm;
+  }
+
   /*
    * Locate the correct property/properties to adjust and update value(s).
    * $xxxx$ names are allowed in any of the property name, attachment name, and attachment index fields
@@ -247,91 +250,40 @@ public class SetPieceProperty extends DynamicProperty {
    */
   @Override
   public Command myKeyEvent(KeyStroke stroke) {
+    final GamePiece outer = Decorator.getOutermost(this);
+    globalSetter.setPropertySource(outer); // Doing this here ensures trait is linked into GamePiece before finding source
+
     Command comm = new NullCommand();
     for (final DynamicKeyCommand keyCommand : keyCommands) {
       if (keyCommand.matches(stroke)) {
-        final String propertyName = (new FormattedString(key)).getText(Decorator.getOutermost(this), this, "Editor.DynamicProperty.property_name");
+        propName = (new FormattedString(key)).getText(Decorator.getOutermost(this), this, "Editor.DynamicProperty.property_name");
 
-        // For evaluating name, we allow either (a) blank means any attachment, (b) string means that attachment name, (c) true/false expression with "AttachmentName" as an available property
-        String attachmentName = (new FormattedString(attachName)).getText(propertiesWithIndex, this, "Editor.SetPieceProperty.attachment_name");
+        changer = keyCommand.propChanger;
+        newValue = null;
 
-        // For evaluating index, we allow either: (a) blank means every index, (b) number means 1-based specific index, or (c) true expression with "AttachmentIndex" and/or "AttachmentBasicName" as an available property
-        String attachmentIndex = (new FormattedString(attachIndex)).getText(propertiesWithIndex, this, "Editor.SetPieceProperty.attachment_index");
+        // Make piece properties filter
+        final AuditTrail audit = AuditTrail.create(this, propertiesFilter.getExpression(), Resources.getString("Editor.GlobalKeyCommand.matching_properties"));
+        PieceFilter filter = propertiesFilter.getFilter(outer, this, audit);
 
-        // Find matching attachments on our piece and set their properties
-        GamePiece p = Decorator.getOutermost(this);
-        while (p instanceof Decorator) {
-          if (p instanceof Attachment) {
-            final Attachment attachment = (Attachment)p;
-
-            currentAttachmentName = attachment.attachName;
-
-            String newValue = null;
-
-            // When we find an Attachment trait, check attachment Name matches
-            boolean match = false;
-            if (attachmentName.isBlank() || attachmentName.equals(attachment.getAttachName())) {
-              match = true;
+        // Make a range filter if applicable
+        if (restrictRange) {
+          int r = range;
+          if (!fixedRange) {
+            final String rangeValue = (String) Decorator.getOutermost(this).getProperty(rangeProperty);
+            try {
+              r = Integer.parseInt(rangeValue);
             }
-            else {
-              // In this case only (a special "Attachment" expression), we need to re-evaluate for each attachment
-              attachmentName = (new FormattedString(attachName)).getText(propertiesWithIndex, this, "Editor.SetPieceProperty.attachment_name");
-              match = "true".equals(attachmentName); //NON-NLS
-            }
-
-            if (match) {
-              index = 0;
-              for (final GamePiece propPiece : attachment.getAttachList()) {
-                index++; // Intentionally first piece is considered index "1"
-
-                currentBasicName = (String)propPiece.getProperty(BasicPiece.BASIC_NAME);
-
-                if (!attachmentIndex.isBlank()) {
-                  if (NumberUtils.isParsable(attachmentIndex)) {
-                    final int parse = NumberUtils.toInt(attachmentIndex);
-                    if ((parse != 0) && (parse != index)) continue;
-                  }
-                  else if (!attachmentIndex.equals(currentBasicName)) {
-                    // In this case only (a special "Index" expression), we need to re-evaluate for each index
-                    attachmentIndex = (new FormattedString(attachIndex)).getText(propertiesWithIndex, this, "Editor.SetPieceProperty.attachment_index");
-                    if (!"true".equals(attachmentIndex)) { //NON-NLS
-                      continue;
-                    }
-                  }
-                }
-
-                GamePiece dp = Decorator.getOutermost(propPiece);
-                while (dp instanceof Decorator) {
-                  if (dp instanceof DynamicProperty) {
-                    final DynamicProperty ourDP = (DynamicProperty)dp;
-
-                    // If we find a matching property, apply our setter on it and we're done
-                    if (propertyName.equals(ourDP.key)) {
-
-                      // If we're doing a prompt, prompt only once. Otherwise re-evaluate setter each time
-                      if ((newValue == null) || !(keyCommand.propChanger instanceof PropertyPrompt)) {
-                        newValue = keyCommand.propChanger.getNewValue(ourDP.value);
-                        // The PropertyChanger has already evaluated any Beanshell, only need to handle any remaining $$variables.
-                        if (newValue.indexOf('$') >= 0) {
-                          format.setFormat(newValue);
-                          newValue = format.getText(propertiesWithIndex, this, "Editor.PropertyChangeConfigurer.new_value");
-                        }
-                      }
-
-                      final ChangeTracker ct = new ChangeTracker(ourDP);
-
-                      ourDP.value = newValue;
-
-                      comm = comm.append(ct.getChangeCommand());
-                    }
-                  }
-                  dp = ((Decorator) dp).getInner();
-                }
-              }
+            catch (NumberFormatException e) {
+              reportDataError(this, Resources.getString("Error.non_number_error"), "range[" + rangeProperty + "]=" + rangeValue, e); // NON-NLS
             }
           }
-          p = ((Decorator) p).getInner();
+          filter = new BooleanAndPieceFilter(filter, new RangeFilter(getMap(), getPosition(), r));
         }
+
+        // Now apply our filter globally & add any matching pieces as attachments
+        comm = comm.append(globalSetter.apply(Map.getMapList().toArray(new Map[0]), filter, target, audit));
+
+        return comm;
       }
     }
     return comm;
@@ -351,8 +303,13 @@ public class SetPieceProperty extends DynamicProperty {
     if (! Objects.equals(encodeConstraints(), c.encodeConstraints())) return false;
     if (! Objects.equals(keyCommandListConfig.getValueString(), c.keyCommandListConfig.getValueString())) return false;
     if (! Objects.equals(description, c.description)) return false;
-    if (! Objects.equals(attachName, c.attachName)) return false;
-    return Objects.equals(attachIndex, c.attachIndex);
+    if (! Objects.equals(target, c.target)) return false;
+    if (! Objects.equals(propertiesFilter, c.propertiesFilter)) return false;
+    if (! Objects.equals(restrictRange, c.restrictRange)) return false;
+    if (! Objects.equals(range, c.range)) return false;
+    if (! Objects.equals(fixedRange, c.fixedRange)) return false;
+    if (! Objects.equals(rangeProperty, c.rangeProperty)) return false;
+    return Objects.equals(globalSetter.getSelectFromDeckExpression(), c.globalSetter.getSelectFromDeckExpression());
   }
 
   protected static class Ed implements PieceEditor {
@@ -445,8 +402,15 @@ public class SetPieceProperty extends DynamicProperty {
       se.append(encodeConstraints());
       se.append(keyCommandListConfig.getValueString());
       se.append(descConfig.getValueString());
-      se.append(attachNameConfig.getValueString());
-      se.append(attachIndexConfig.getValueString());
+
+      se.append(target.encode());
+      se.append(propertiesFilter);
+      se.append(restrictRange);
+      se.append(range);
+      se.append(fixedRange);
+      se.append(rangeProperty);
+      se.append(globalSetter.getSelectFromDeckExpression());
+
       return ID + se.getValue();
     }
 
