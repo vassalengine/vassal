8e4c9dc320b0a3b89ac6bac913b2eb5c35aa168a
Joel Uckelman <uckelman@nomic.net>
2023-05-08 15:46:34 +0100

    Formatting.

--- vassal-app/src/main/java/VASSAL/build/module/map/PieceMover.java
+++ vassal-app/src/main/java/VASSAL/build/module/map/PieceMover.java
@@ -1858,7 +1858,8 @@ public class PieceMover extends AbstractBuildable
         piecePosition = mousePosition;
       }
 
-      // If coming from a map, we use the map's zoom. Otherwise if our PieceWindow has stashed a starting scale for us then use that, else 1.0
+      // If coming from a map, we use the map's zoom. Otherwise if our
+      // PieceWindow has stashed a starting scale for us then use that, else 1.0
       if (map != null) {
         dragPieceOffCenterZoom = map.getZoom();
       }
