1419fffc668976d60e217152fa1dde52bed35cc3
Joel Uckelman <uckelman@nomic.net>
2023-05-08 21:39:38 +0100

    Formatting.

--- vassal-app/src/main/java/VASSAL/build/module/map/PieceMover.java
+++ vassal-app/src/main/java/VASSAL/build/module/map/PieceMover.java
@@ -1831,7 +1831,8 @@ public class PieceMover extends AbstractBuildable
                                   .relativePosition(piece.getParent(), piece);
         piecePosition.translate(
           (int) Math.round(offset.x * dragPieceOffCenterZoom),
-          (int) Math.round(offset.y * dragPieceOffCenterZoom));
+          (int) Math.round(offset.y * dragPieceOffCenterZoom)
+        );
       }
 
       // dragging from UL results in positive offsets
