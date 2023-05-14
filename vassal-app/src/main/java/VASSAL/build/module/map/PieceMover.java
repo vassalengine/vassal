b8f71676f92704acd2e6ba783b0e2b0605e7cbde
Joel Uckelman <uckelman@nomic.net>
2023-05-14 22:35:38 +0100

    Calling calcDrawOffset() when only the first piece has contributed to the boundingBox is probably wrong.

--- vassal-app/src/main/java/VASSAL/build/module/map/PieceMover.java
+++ vassal-app/src/main/java/VASSAL/build/module/map/PieceMover.java
@@ -1610,9 +1610,6 @@ public class PieceMover extends AbstractBuildable
       boundingBox.height *= zoom;
       boundingBox.x *= zoom;
       boundingBox.y *= zoom;
-      if (doOffset) {
-        calcDrawOffset();
-      }
 
       relativePositions.add(new Point(0, 0));
       int stackCount = 0;
@@ -1643,6 +1640,10 @@ public class PieceMover extends AbstractBuildable
         boundingBox.add(r);
         relativePositions.add(p);
         lastPiece = nextPiece;
+      }
+
+      if (doOffset) {
+        calcDrawOffset();
       }
 
       return relativePositions;
