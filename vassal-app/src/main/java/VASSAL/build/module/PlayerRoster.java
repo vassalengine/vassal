405dd19102a13b52eb2d8ac77dc5d3dcf023c57a
riverwanderer <58135975+riverwanderer@users.noreply.github.com>
2023-09-09 08:28:24 +0100

    Remove redundant setting of availableSides in finish().

--- vassal-app/src/main/java/VASSAL/build/module/PlayerRoster.java
+++ vassal-app/src/main/java/VASSAL/build/module/PlayerRoster.java
@@ -470,11 +470,9 @@ public class PlayerRoster extends AbstractToolbarItem implements CommandEncoder,
     }
 
     final String newSide = untranslateSide(sideConfig.getValueString());
-
-    // Check if attempting switch to side taken whilst dialog was active. If found, return without setting side i.e. pickedSide = false.
-    final ArrayList<String> availableSides = new ArrayList<>(sides);
     final ArrayList<String> alreadyTaken = new ArrayList<>();
 
+    // Check if attempting switch to side taken whilst dialog was active. If found, return without setting side i.e. pickedSide = false.
     for (final PlayerInfo p : players) {
       alreadyTaken.add(p.side);
     }
