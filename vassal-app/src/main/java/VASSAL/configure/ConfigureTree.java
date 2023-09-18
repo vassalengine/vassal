71932d0b506e4d351139627a71c4f25d3705cb45
riverwanderer <58135975+riverwanderer@users.noreply.github.com>
2023-09-18 04:51:11 +0100

    Regex search string patch.

--- vassal-app/src/main/java/VASSAL/configure/ConfigureTree.java
+++ vassal-app/src/main/java/VASSAL/configure/ConfigureTree.java
@@ -2662,6 +2662,10 @@ public class ConfigureTree extends JTree implements PropertyChangeListener, Mous
      */
     private boolean checkString(String target, String searchString) {
       if (searchParameters.isMatchRegex()) {
+        // patch search string to avoid exception when Regex starts with * only
+        if (searchString.substring(0,1) == "*") {
+          searchString = "." + searchString;
+        }
         if (searchParameters.isMatchCase()) {
           return target.matches(searchString);
         }
