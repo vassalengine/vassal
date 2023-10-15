5856e6954999e318ddffe4fd71641151e0c9b1b1
Brian Reynolds <brian@brianreynolds.net>
2023-10-15 16:36:57 -0400

    but don't run that part on windows

--- vassal-app/src/main/java/VASSAL/configure/NamedHotKeyConfigurer.java
+++ vassal-app/src/main/java/VASSAL/configure/NamedHotKeyConfigurer.java
@@ -24,6 +24,7 @@ import VASSAL.tools.icon.IconFactory;
 import VASSAL.tools.icon.IconFamily;
 import VASSAL.tools.swing.SwingUtils;
 import net.miginfocom.swing.MigLayout;
+import org.apache.commons.lang3.SystemUtils;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -425,12 +426,14 @@ public class NamedHotKeyConfigurer extends Configurer implements FocusListener {
       switch (e.getKeyCode()) {
       case KeyEvent.VK_DELETE:
       case KeyEvent.VK_BACK_SPACE:
-        // Allow mapping of Delete
-        if (getValue().equals(NamedKeyStroke.NULL_KEYSTROKE) || e.isShiftDown() || e.isControlDown() || e.isMetaDown() || e.isAltDown()) {
-          setValue(NamedKeyStroke.of(SwingUtils.convertKeyEvent(e)));
-        }
-        else {
-          setValue(NamedKeyStroke.NULL_KEYSTROKE);
+        if (SystemUtils.IS_OS_MAC) {
+          // Allow mapping of Delete
+          if (getValue().equals(NamedKeyStroke.NULL_KEYSTROKE) || e.isShiftDown() || e.isControlDown() || e.isMetaDown() || e.isAltDown()) {
+            setValue(NamedKeyStroke.of(SwingUtils.convertKeyEvent(e)));
+          }
+          else {
+            setValue(NamedKeyStroke.NULL_KEYSTROKE);
+          }
         }
         break;
       case KeyEvent.VK_SHIFT:
