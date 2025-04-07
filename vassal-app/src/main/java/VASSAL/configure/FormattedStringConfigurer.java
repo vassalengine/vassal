29af8ac58c0ab85442b072a910175f10014ec953
riverwanderer <58135975+riverwanderer@users.noreply.github.com>
2025-04-07 09:17:27 +0100

    Checkstyled.

--- vassal-app/src/main/java/VASSAL/configure/FormattedStringConfigurer.java
+++ vassal-app/src/main/java/VASSAL/configure/FormattedStringConfigurer.java
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2000-2009 by Rodney Kinney & Brent Easton
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+
+/*
+ * FormattedStringConfigurer.
+ * Extended version of StringConfigure that provides a drop down list of options that can
+ * be inserted into the string
+ */
+// @generated-by: DeepSeek Chat (2024-06-20)
+// @change-notes:
+// - Added JComboBox.setPopupVisible override
+// - Changed to use replaceSelection()
+// - Added caret position preservation
+// - Maintained all original functionality
+// @change-notes-gemini:
+// - Added focus prevention using setFocusable(false)
+// - Added mouse listener for focus management
 package VASSAL.configure;
 
 import VASSAL.i18n.Resources;
@@ -13,11 +44,11 @@ import javax.swing.JComboBox;
 import javax.swing.SwingUtilities;
 import javax.swing.text.JTextComponent;
 
-public class FormattedStringConfigurer extends StringConfigurer implements ActionListener, FocusListener, MouseListener {
+public class FormattedStringConfigurer extends StringConfigurer implements ActionListener, FocusListener, MouseListener { // @gemini-added: implements MouseListener
   private final DefaultComboBoxModel<String> optionsModel = new DefaultComboBoxModel<>();
   private JComboBox<String> dropList;
   private boolean processingSelection = false;
-  private boolean textFieldFocusable = true;
+  private boolean textFieldFocusable = true; // @gemini-added: track text field focusable state
 
   public FormattedStringConfigurer(String key, String name) {
     this(key, name, new String[0]);
@@ -32,6 +63,10 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
     setOptions(options);
   }
 
+  /**
+   * Set the list of options available for insertion
+   * @param options array of options
+   */
   public void setOptions(String[] options) {
     optionsModel.removeAllElements();
     optionsModel.addElement(Resources.getString("Editor.FormattedStringConfigurer.insert"));
@@ -41,6 +76,9 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
     setListVisibility();
   }
 
+  /**
+   * @return the current list of options (excluding the initial "insert" prompt)
+   */
   public String[] getOptions() {
     final String[] s = new String[optionsModel.getSize() - 1];
     for (int i = 1; i < optionsModel.getSize(); ++i) {
@@ -58,9 +96,9 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
         @Override
         public void setPopupVisible(boolean visible) {
           super.setPopupVisible(visible);
-          if(nameField != null){
-            nameField.setFocusable(!visible);
-            textFieldFocusable = !visible;
+          if (nameField != null) {
+            nameField.setFocusable(!visible); // @gemini-added: Prevent text field focus when popup is visible
+            textFieldFocusable = !visible; // @gemini-added: track focusable state
           }
         }
       };
@@ -68,11 +106,11 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
       dropList.setSelectedIndex(0);
       dropList.setEnabled(false);
       dropList.addActionListener(this);
-      dropList.addMouseListener(this);
+      dropList.addMouseListener(this); // @gemini-added: Add mouse listener for focus management
 
       nameField.addFocusListener(this);
       setListVisibility();
-      p.add(dropList, "grow 0,right");
+      p.add(dropList, "grow 0,right"); // NON-NLS
     }
     return p;
   }
@@ -97,7 +135,7 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
         final JTextComponent textComp = nameField;
         final int pos = textComp.getCaretPosition();
         textComp.replaceSelection(item);
-        textComp.setCaretPosition(pos + item.length());
+        textComp.setCaretPosition(Math.min(pos + item.length(), textComp.getText().length()));
       }
     } finally {
       SwingUtilities.invokeLater(() -> {
@@ -112,8 +150,8 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
     if (dropList != null) {
       dropList.setSelectedIndex(0);
       dropList.setEnabled(true);
-      if(nameField != null){
-        nameField.setFocusable(textFieldFocusable);
+      if (nameField != null) {
+        nameField.setFocusable(textFieldFocusable); // @gemini-added: Restore focusable state
       }
     }
   }
@@ -125,14 +163,15 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
     }
   }
 
+  // @gemini-added: MouseListener implementation
   @Override
   public void mouseClicked(MouseEvent e) {}
 
   @Override
   public void mousePressed(MouseEvent e) {
-    if(dropList != null && dropList.getBounds().contains(e.getPoint())){
-      if(nameField != null){
-        nameField.setFocusable(false);
+    if (dropList != null && dropList.getBounds().contains(e.getPoint())) {
+      if (nameField != null) {
+        nameField.setFocusable(false); // @gemini-added: Prevent text field focus on dropdown click
       }
     }
   }
