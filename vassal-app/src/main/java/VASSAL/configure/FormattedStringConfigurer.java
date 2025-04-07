9049f08c094876d11f409fd45ac3e1298f6b378e
riverwanderer <58135975+riverwanderer@users.noreply.github.com>
2025-04-07 09:11:25 +0100

    Revised effort (Gemini).

--- vassal-app/src/main/java/VASSAL/configure/FormattedStringConfigurer.java
+++ vassal-app/src/main/java/VASSAL/configure/FormattedStringConfigurer.java
@@ -1,31 +1,3 @@
-/*
- * Copyright (c) 2000-2009 by Rodney Kinney & Brent Easton
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License (LGPL) as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, copies are available
- * at http://www.opensource.org.
- */
-
-/*
- * FormattedStringConfigurer.
- * Extended version of StringConfigure that provides a drop down list of options that can
- * be inserted into the string
- */
-// @generated-by: DeepSeek Chat (2024-06-20)
-// @change-notes:
-// - Added JComboBox.setPopupVisible override
-// - Changed to use replaceSelection()
-// - Added caret position preservation
-// - Maintained all original functionality
 package VASSAL.configure;
 
 import VASSAL.i18n.Resources;
@@ -34,16 +6,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.SwingUtilities;
 import javax.swing.text.JTextComponent;
 
-public class FormattedStringConfigurer extends StringConfigurer implements ActionListener, FocusListener {
+public class FormattedStringConfigurer extends StringConfigurer implements ActionListener, FocusListener, MouseListener {
   private final DefaultComboBoxModel<String> optionsModel = new DefaultComboBoxModel<>();
   private JComboBox<String> dropList;
   private boolean processingSelection = false;
-  private boolean skipTextSelect = false;
+  private boolean textFieldFocusable = true;
 
   public FormattedStringConfigurer(String key, String name) {
     this(key, name, new String[0]);
@@ -58,10 +32,6 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
     setOptions(options);
   }
 
-  /**
-   * Set the list of options available for insertion
-   * @param options array of options
-   */
   public void setOptions(String[] options) {
     optionsModel.removeAllElements();
     optionsModel.addElement(Resources.getString("Editor.FormattedStringConfigurer.insert"));
@@ -71,9 +41,6 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
     setListVisibility();
   }
 
-  /**
-   * @return the current list of options (excluding the initial "insert" prompt)
-   */
   public String[] getOptions() {
     final String[] s = new String[optionsModel.getSize() - 1];
     for (int i = 1; i < optionsModel.getSize(); ++i) {
@@ -90,32 +57,22 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
       dropList = new JComboBox<>(optionsModel) {
         @Override
         public void setPopupVisible(boolean visible) {
-          skipTextSelect = visible;
-          if (!processingSelection && nameField != null) {
-            final JTextComponent tc = nameField;
-            final int pos = tc.getCaretPosition();
-            SwingUtilities.invokeLater(() -> tc.setCaretPosition(
-                    Math.min(pos, tc.getText().length())
-            ));
-          }
           super.setPopupVisible(visible);
+          if(nameField != null){
+            nameField.setFocusable(!visible);
+            textFieldFocusable = !visible;
+          }
         }
       };
 
       dropList.setSelectedIndex(0);
       dropList.setEnabled(false);
       dropList.addActionListener(this);
-
-      nameField.addCaretListener(e -> {
-        if (skipTextSelect) {
-          nameField.setSelectionStart(e.getDot());
-          nameField.setSelectionEnd(e.getDot());
-        }
-      });
+      dropList.addMouseListener(this);
 
       nameField.addFocusListener(this);
       setListVisibility();
-      p.add(dropList, "grow 0,right"); // NON-NLS
+      p.add(dropList, "grow 0,right");
     }
     return p;
   }
@@ -140,12 +97,9 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
         final JTextComponent textComp = nameField;
         final int pos = textComp.getCaretPosition();
         textComp.replaceSelection(item);
-        textComp.setCaretPosition(
-                Math.min(pos + item.length(), textComp.getText().length())
-        );
+        textComp.setCaretPosition(pos + item.length());
       }
-    }
-    finally {
+    } finally {
       SwingUtilities.invokeLater(() -> {
         dropList.setSelectedIndex(0);
         processingSelection = false;
@@ -158,6 +112,9 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
     if (dropList != null) {
       dropList.setSelectedIndex(0);
       dropList.setEnabled(true);
+      if(nameField != null){
+        nameField.setFocusable(textFieldFocusable);
+      }
     }
   }
 
@@ -167,4 +124,25 @@ public class FormattedStringConfigurer extends StringConfigurer implements Actio
       dropList.setEnabled(false);
     }
   }
+
+  @Override
+  public void mouseClicked(MouseEvent e) {}
+
+  @Override
+  public void mousePressed(MouseEvent e) {
+    if(dropList != null && dropList.getBounds().contains(e.getPoint())){
+      if(nameField != null){
+        nameField.setFocusable(false);
+      }
+    }
+  }
+
+  @Override
+  public void mouseReleased(MouseEvent e) {}
+
+  @Override
+  public void mouseEntered(MouseEvent e) {}
+
+  @Override
+  public void mouseExited(MouseEvent e) {}
 }
