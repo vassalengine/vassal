44c42372c03a1df43e1771015bbaf066d730db27
Artur Łysik <artur.lysik@cloudimed.pl>
2025-04-16 08:23:14 +0200

    fix(13036): Show image file name for button icons - reformatted with checkstyle

--- vassal-app/src/main/java/VASSAL/configure/button/ToolTipConfigurer.java
+++ vassal-app/src/main/java/VASSAL/configure/button/ToolTipConfigurer.java
@@ -8,15 +8,14 @@ import javax.swing.ImageIcon;
 
 public class ToolTipConfigurer {
 
-    private ToolTipConfigurer() {
-    }
+  private ToolTipConfigurer() {
+  }
 
-    public static void addToolTipToButtonIcon(String promptValue, Configurer config) {
-        if (Resources.getString(Resources.BUTTON_ICON).equals(promptValue)
-                && config instanceof IconConfigurer
-                && ((IconConfigurer) config).getIconValue() instanceof ImageIcon) {
-            ((IconConfigurer) config).setToolTipText(config.getValueString());
-        }
+  public static void addToolTipToButtonIcon(String promptValue, Configurer config) {
+    if (Resources.getString(Resources.BUTTON_ICON).equals(promptValue) && config instanceof IconConfigurer &&
+      ((IconConfigurer) config).getIconValue() instanceof ImageIcon) {
+      ((IconConfigurer) config).setToolTipText(config.getValueString());
     }
+  }
 
 }
