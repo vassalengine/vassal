6db75b051254ee103827001205f082dafc879612
Joel Uckelman <uckelman@nomic.net>
2026-09-15 21:17:25 +0100

    Use the TileRenderer in Chart.

--- vassal-app/src/main/java/VASSAL/build/widget/Chart.java
+++ vassal-app/src/main/java/VASSAL/build/widget/Chart.java
@@ -19,11 +19,17 @@ package VASSAL.build.widget;
 
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 import java.io.File;
 import java.util.Collection;
 
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import VASSAL.build.Buildable;
@@ -32,9 +38,11 @@ import VASSAL.build.module.documentation.HelpFile;
 import VASSAL.i18n.Resources;
 import VASSAL.tools.AdjustableSpeedScrollPane;
 import VASSAL.tools.DataArchive;
+import VASSAL.tools.image.TileRenderer;
 import VASSAL.tools.imageop.Op;
-import VASSAL.tools.imageop.OpIcon;
+import VASSAL.tools.imageop.ScaleOp;
 import VASSAL.tools.imageop.SourceOp;
+import VASSAL.tools.swing.SwingUtils;
 
 /**
  * A Chart is used for displaying charts and tables for the module.
@@ -48,25 +56,108 @@ public class Chart extends Widget {
   private Component chart;
   private String fileName;
   private SourceOp srcOp;
-  private JLabel label;
+  private ScaleOp scaleOp;
+  private View view;
+
+  private static final Point2D ORIGIN = new Point2D.Double(0.0, 0.0);
+
+//  private double zoom = 1.0;
+  private static final double zoom = 1.0;
+  private final Rectangle boundaries = new Rectangle();
+
+  private final TileRenderer renderer = new TileRenderer();
+
+/*
+  public void setZoom(double z) {
+    zoom = z;
+  }
+*/
+
+  private class View extends JPanel {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public Dimension getPreferredSize() {
+      final Dimension size = srcOp.getSize();
+      size.width *= zoom;
+      size.height *= zoom;
+      return size;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+      final Graphics2D g2d = (Graphics2D) g;
+
+      g2d.addRenderingHints(SwingUtils.FONT_HINTS);
+      g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                           RenderingHints.VALUE_ANTIALIAS_ON);
+
+      final double os_scale = g2d.getDeviceConfiguration().getDefaultTransform().getScaleX();
+
+      // HDPI: We may get a transform where scale != 1. This means we
+      // are running on an HDPI system. We want to draw at the effective
+      // scale factor to prevent poor quality upscaling, so reset the
+      // transform to scale of 1 and multiply the map zoom by the OS scaling.
+      final AffineTransform orig_t = g2d.getTransform();
+      g2d.setTransform(SwingUtils.descaleTransform(orig_t));
+
+      final Rectangle vr = getVisibleRect();
+
+      final Rectangle r = new Rectangle(
+        (int)(vr.x * os_scale),
+        (int)(vr.y * os_scale),
+        (int)(vr.width * os_scale),
+        (int)(vr.height * os_scale)
+      );
+
+      g2d.setColor(getBackground());
+      g2d.fillRect(r.x, r.y, r.width, r.height);
+
+      final double dzoom = zoom * os_scale;
+
+      scaleOp = renderer.renderRegion(
+        g,
+        ORIGIN,
+        r,
+        dzoom,
+        boundaries,
+        1.0,
+        srcOp,
+        scaleOp,
+        null,
+        false,
+        false,
+        null,
+        null, // FIXME: map
+        this
+      );
+
+      g2d.setTransform(orig_t);
+    }
+
+    @Override
+    public void update(Graphics g) {
+      // To avoid flicker, don't clear the display first
+      paint(g);
+    }
+  }
 
   @Override
   public Component getComponent() {
     if (chart == null) {
-      label = new JLabel();
+      view = new View();
       srcOp = (fileName == null || fileName.isBlank()) ? null : Op.load(fileName);
-      if (srcOp != null) {
-        label.setIcon(new OpIcon(srcOp));
-      }
-      final Dimension d = label.getPreferredSize();
+      boundaries.setSize(srcOp.getSize());
+
+      final Dimension d = view.getPreferredSize();
       if (d.width > 300 || d.height > 300) {
-        final JScrollPane scroll = new AdjustableSpeedScrollPane(label);
-        scroll.getViewport().setPreferredSize(label.getPreferredSize());
+        final JScrollPane scroll = new AdjustableSpeedScrollPane(view);
+        scroll.getViewport().setPreferredSize(view.getPreferredSize());
         scroll.getViewport().setAlignmentY(0.0F);
         chart = scroll;
       }
       else {
-        chart = label;
+        chart = view;
       }
     }
     return chart;
@@ -99,11 +190,10 @@ public class Chart extends Widget {
         val = ((File) val).getName();
       }
       fileName = (String) val;
-      if (label != null) {
+      if (view != null) {
         srcOp = (fileName == null || fileName.isBlank()) ? null : Op.load(fileName);
         if (srcOp != null) {
-          label.setIcon(new OpIcon(srcOp));
-          label.revalidate();
+          view.revalidate();
         }
       }
     }
